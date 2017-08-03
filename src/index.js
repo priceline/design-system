b70498ef90c0cd05aa93078d80fe6af0b991073c
Brent Jackson <jxnblk@gmail.com>
2017-08-03 15:12:43 -0400

    Add Box story

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export { default as Text } from './Text'
+export { default as Box } from './Box'
+
 export { default as theme } from './theme'
 export { default as legacyTheme } from './legacyTheme'
 export { default as ThemeProvider } from './ThemeProvider'
 export * from './theme'
-export { default as Box } from './Box'
