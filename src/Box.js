ec749c303a0d05a8efb99a1101f2f27bbd234b39
Brent Jackson <jxnblk@gmail.com>
2017-08-03 15:02:23 -0400

    Add styled-system props to Box component

--- src/Box.js
+++ src/Box.js
@@ -1,7 +1,40 @@
 import styled from 'styled-components'
+import { space, width, color } from 'styled-system'
+import PropTypes from 'prop-types'
 
 const Box = styled.div`
-  
+  ${space}
+  ${width}
+  ${color}
 `
+
+const numberStringOrArray = PropTypes.oneOfType([
+  PropTypes.number,
+  PropTypes.string,
+  PropTypes.array
+])
+
+Box.propTypes = {
+  color: PropTypes.string,
+  bg: PropTypes.string,
+  width: numberStringOrArray,
+  w: numberStringOrArray,
+  /** Margin */
+  m: numberStringOrArray,
+  mt: numberStringOrArray,
+  mr: numberStringOrArray,
+  mb: numberStringOrArray,
+  ml: numberStringOrArray,
+  mx: numberStringOrArray,
+  my: numberStringOrArray,
+  /** Padding */
+  p: numberStringOrArray,
+  pt: numberStringOrArray,
+  pr: numberStringOrArray,
+  pb: numberStringOrArray,
+  pl: numberStringOrArray,
+  px: numberStringOrArray,
+  py: numberStringOrArray,
+}
 
 export default Box
