fae06838e00906e4b46cc0ef5aae2380976b112c
Jackson, Brent <Brent.Jackson@priceline.com>
2017-08-10 12:27:00 -0400

    Add Icon story and tests

---
+++ src/Icon.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components'
+import { space, color } from 'styled-system'
+import PropTypes from 'prop-types'
+import icons from '../icons.json'
+
+const Base = ({
+  name,
+  size,
+  ...props
+}) => {
+  const icon = icons[name]
+  if (!icon) return false
+
+  return (
+    <svg
+      {...props}
+      viewBox={icon.viewBox}
+      width={size}
+      height={size}
+      fill='currentcolor'>
+      <path d={icon.path} />
+    </svg>
+  )
+}
+
+const Icon = styled(Base)`
+  ${space}
+  ${color}
+`
+
+Icon.defaultProps = {
+  name: 'checkLight',
+  size: 24
+}
+
+Icon.propTypes = {
+  name: PropTypes.oneOf(Object.keys(icons)).isRequired,
+  size: PropTypes.number
+}
+
+export default Icon
