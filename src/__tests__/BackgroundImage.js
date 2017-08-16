c77ad0a50ac7223be380ff669a3fabe488ca6f2b
Brent Jackson <jxnblk@gmail.com>
2017-08-16 10:07:05 -0400

    Add story and basic docs for BackgroundImage

---
+++ src/__tests__/BackgroundImage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
+import { BackgroundImage, theme } from '..'
+
+describe('BackgroundImage', () => {
+  test('renders', () => {
+    const json = renderer.create(<BackgroundImage theme={theme} />).toJSON()
+    expect(json).toMatchSnapshot()
+  })
+
+  test('renders with image', () => {
+    const json = renderer.create(<BackgroundImage image='hello.png' theme={theme} />).toJSON()
+    expect(json).toMatchSnapshot()
+  })
+
+  test('renders with height', () => {
+    const json = renderer.create(<BackgroundImage height='320px' theme={theme} />).toJSON()
+    expect(json).toMatchSnapshot()
+  })
+})
