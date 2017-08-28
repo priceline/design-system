890ed84b1a50be033aeb705f24427e40bb3dc7ab
Ben Chen <benjaminlchen@gmail.com>
2017-08-28 12:07:57 -0400

    Refactor the Heading component to refine default props

--- src/Heading.js
+++ src/Heading.js
@@ -1,42 +1,27 @@
 import Text from './Text'
-import { fontSizes } from './theme'
 
 const Heading = Text.withComponent('h3')
 
 Heading.displayName = 'Heading'
 
-Heading.defaultProps = {
-  fontSize: fontSizes[4]
-}
+Heading.defaultProps = { fontSize: 4, m: 0 }
 
 Heading.h1 = Heading.withComponent('h1')
-Heading.h1.defaultProps = {
-  fontSize: fontSizes[6]
-}
+Heading.h1.defaultProps = { fontSize: 6, m: 0 }
 
 Heading.h2 = Heading.withComponent('h2')
-Heading.h2.defaultProps = {
-  fontSize: fontSizes[5]
-}
+Heading.h2.defaultProps = { fontSize: 5, m: 0 }
 
 Heading.h3 = Heading.withComponent('h3')
-Heading.h3.defaultProps = {
-  fontSize: fontSizes[4]
-}
+Heading.h3.defaultProps = { fontSize: 4, m: 0 }
 
 Heading.h4 = Heading.withComponent('h4')
-Heading.h4.defaultProps = {
-  fontSize: fontSizes[3]
-}
+Heading.h4.defaultProps = { fontSize: 3, m: 0 }
 
 Heading.h5 = Heading.withComponent('h5')
-Heading.h5.defaultProps = {
-  fontSize: fontSizes[2]
-}
+Heading.h5.defaultProps = { fontSize: 2, m: 0 }
 
 Heading.h6 = Heading.withComponent('h6')
-Heading.h6.defaultProps = {
-  fontSize: fontSizes[1]
-}
+Heading.h6.defaultProps = { fontSize: 1, m: 0 }
 
 export default Heading
