2832c83193c721a31b9fed058e0d71a4babddeb6
Ben Chen <benjaminlchen@gmail.com>
2017-09-05 20:33:20 -0400

    Update default color of Link component

--- src/Link.js
+++ src/Link.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
+import { colors } from './theme'
 
 const Link = styled.a`
   text-decoration: none;
+  color: ${colors['blue']};
   &:hover {
     text-decoration: underline;
   }
