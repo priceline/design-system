9702c63461ffec1d7f3dd11f76d43f649029da28
Ben Chen <benjaminlchen@gmail.com>
2017-09-25 22:37:42 -0400

    Remove Unneeded Istanbul Coverage Ignore

--- src/helpers/webp-support.js
+++ src/helpers/webp-support.js
@@ -4,6 +4,5 @@ canvasMock.width = canvasMock.height = 1
 // If canvas supports webp, the returned Data URLs would  be prefixed with "data:".
 // Thus data starts at index 5, after the prefix.
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs
-/* istanbul ignore next */
 export const detectWebPSupport = () => canvasMock.toDataURL
   ? canvasMock.toDataURL('image/webp').indexOf('image/webp') === 5 : false
