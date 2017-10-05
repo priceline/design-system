2745f78fa9b4aacc86cf83c4d5e7560f01907810
Brent Jackson <jxnblk@gmail.com>
2017-10-05 11:27:35 -0400

    Clean up storybook demo

--- .storybook/Hide.js
+++ .storybook/Hide.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { Hide } from '../src'
+import { Hide, Flex } from '../src'
 
 storiesOf('Hide', module)
   .add('Hide', () => (
-    <div>
-      <Hide xs>Hide xs</Hide>
-      <Hide sm>Hide sm</Hide>
-      <Hide md>Hide md</Hide>
-      <Hide lg>Hide lg</Hide>
-      <Hide xl>Hide xl</Hide>
-    </div>
+    <Flex justify='space-between'>
+      <Hide xs p={2} bg='red'>Hide xs</Hide>
+      <Hide sm p={2} bg='orange'>Hide sm</Hide>
+      <Hide md p={2} bg='green'>Hide md</Hide>
+      <Hide lg p={2} bg='blue'>Hide lg</Hide>
+      <Hide xl p={2} bg='purple'>Hide xl</Hide>
+    </Flex>
   ))
