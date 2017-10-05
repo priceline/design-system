356e03840748524bb6c94f1d9440da35ddf6eb4d
Brent Jackson <jxnblk@gmail.com>
2017-10-05 11:29:31 -0400

    Add tests

--- src/__tests__/Hide.js
+++ src/__tests__/Hide.js
@@ -8,4 +8,30 @@ describe('Hide', () => {
     const json = renderer.create(<Hide />).toJSON()
     expect(json).toMatchSnapshot()
   })
+
+  test('renders with xs prop', () => {
+    const json = renderer.create(<Hide xs theme={theme} />).toJSON()
+    expect(json).toMatchSnapshot()
+  })
+
+  test('renders with sm prop', () => {
+    const json = renderer.create(<Hide sm theme={theme} />).toJSON()
+    expect(json).toMatchSnapshot()
+  })
+
+  test('renders with md prop', () => {
+    const json = renderer.create(<Hide md theme={theme} />).toJSON()
+    expect(json).toMatchSnapshot()
+  })
+
+  test('renders with lg prop', () => {
+    const json = renderer.create(<Hide lg theme={theme} />).toJSON()
+    expect(json).toMatchSnapshot()
+  })
+
+  test('renders with xl prop', () => {
+    const json = renderer.create(<Hide xl theme={theme} />).toJSON()
+    expect(json).toMatchSnapshot()
+  })
+
 })
