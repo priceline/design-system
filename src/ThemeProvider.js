b89c1cd2ad417d32b51e2dc19daac23d596a0430
Jennifer Cen <cenjennifer@gmail.com>
2017-10-11 14:59:13 -0400

    remove unintentional attr change

--- src/ThemeProvider.js
+++ src/ThemeProvider.js
@@ -23,7 +23,7 @@ const ThemeProvider = ({ legacy, ...props }) => {
   const theme = legacy ? legacyTheme : nextTheme
 
   return (
-    <StyledThemeProvider theme={theme} hello="hello">
+    <StyledThemeProvider theme={theme}>
       <Base {...props} />
     </StyledThemeProvider>
   )
