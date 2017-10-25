4ee06acc8f9115fb68c1584e622ca888e90e5966
Jon Schwarz <jes708@nyu.edu>
2017-10-25 14:00:07 -0400

    feat(hug): remove enzyme

--- src/__tests__/Hug.js
+++ src/__tests__/Hug.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import { shallow } from 'enzyme'
-import toJson from 'enzyme-to-json'
 import 'jest-styled-components'
 import { Hug, Text, Card, theme } from '..'
 
-const text = <Text.span>Have you seen this <Text.span bold>card</Text.span> I'm hugging?!</Text.span>
+const text = (
+  <Text.span>
+    Have you seen this <Text.span bold>card</Text.span> I'm hugging?!
+  </Text.span>
+)
 
 describe('Hug', () => {
   test('renders with border-radius from theme', () => {
@@ -15,29 +17,39 @@ describe('Hug', () => {
   })
 
   test('renders text, icon, and Child', () => {
-    const json = toJson(shallow(
-      <Hug icon='closeLight' text={text} theme={theme}>
-        <Card p={3} bg='white' color='text'>I'm a card within a hug!</Card>
+    const json = renderer.create(
+      <Hug icon="closeLight" text={text} theme={theme}>
+        <Card p={3} bg="white" color="text" theme={theme}>
+          I'm a card within a hug!
+        </Card>
       </Hug>
-    ))
+    ).toJSON
     expect(json).toMatchSnapshot()
   })
 
   test('renders text when string is provided', () => {
-    const json = toJson(shallow(
-      <Hug icon='closeLight' text="I've been known to hug a card from time to time" theme={theme}>
-        <Card p={3} bg='white' color='text'>I'm a card within a hug!</Card>
+    const json = renderer.create(
+      <Hug
+        icon="closeLight"
+        text="I've been known to hug a card from time to time"
+        theme={theme}
+      >
+        <Card p={3} bg="white" color="text" theme={theme}>
+          I'm a card within a hug!
+        </Card>
       </Hug>
-    ))
+    ).toJSON
     expect(json).toMatchSnapshot()
   })
 
   test('renders text when array of nodes is provided', () => {
-    const json = toJson(shallow(
-      <Hug icon='closeLight' text={[text, text]} theme={theme}>
-        <Card p={3} bg='white' color='text'>I'm a card within a hug!</Card>
+    const json = renderer.create(
+      <Hug icon="closeLight" text={[text, text]} theme={theme}>
+        <Card p={3} bg="white" color="text" theme={theme}>
+          I'm a card within a hug!
+        </Card>
       </Hug>
-    ))
+    ).toJSON
     expect(json).toMatchSnapshot()
   })
 })
