f91b189b89ad9442221f690f1219f6109923a83a
Brent Jackson <jxnblk@gmail.com>
2017-11-01 17:21:07 -0400

    Move new icons and organize into folders

--- scripts/parse-icons.js
+++ scripts/parse-icons.js
@@ -3,7 +3,8 @@ const path = require('path')
 const SVGI = require('svgi')
 const camelCase = require('camelcase')
 
-const src = path.join(__dirname, '..', 'icons')
+const legacySrc = path.join(__dirname, '..', 'icons/legacy')
+const newSrc = path.join(__dirname, '..', 'icons/new')
 const filepath = path.join(__dirname, '..', 'icons.json')
 
 const writeFile = data => {
@@ -11,18 +12,20 @@ const writeFile = data => {
   fs.writeFileSync(filepath, json)
 }
 
-const readIcons = dir => fs.readdirSync(dir)
-  .filter(file => /\.svg$/.test(file))
-  .map(file => {
-    const name = path.basename(file, '.svg')
-    const key = camelCase(removePrefix(name))
-    const svg = fs.readFileSync(path.join(src, file), 'utf8')
+const readIcons = dir =>
+  fs
+    .readdirSync(dir)
+    .filter(file => /\.svg$/.test(file))
+    .map(file => {
+      const name = path.basename(file, '.svg')
+      const key = camelCase(removePrefix(name))
+      const svg = fs.readFileSync(path.join(dir, file), 'utf8')
 
-    return {
-      key,
-      svg
-    }
-  })
+      return {
+        key,
+        svg
+      }
+    })
 
 const parseSVG = svg => {
   const { nodes } = new SVGI(svg).report()
@@ -37,30 +40,37 @@ const parseSVG = svg => {
 
 const flattenChildren = (a, b) => {
   const children = b.children.reduce(flattenChildren, [])
-  return [ ...a, b, ...children ]
+  return [...a, b, ...children]
 }
 
-const getPath = nodes => nodes.children
-  .reduce(flattenChildren, [])
-  .filter(child => child.type === 'path')
-  .map(child => child.properties.d)
-  .join(' ')
+const getPath = nodes =>
+  nodes.children
+    .reduce(flattenChildren, [])
+    .filter(child => child.type === 'path')
+    .map(child => child.properties.d)
+    .join(' ')
 
 const getViewBox = nodes => nodes.properties.viewBox
 
 const removePrefix = str => str.replace(/^ic-/, '')
 
-const getData = icons => icons.map(icon => Object.assign({}, icon, parseSVG(icon.svg)))
+const getData = icons =>
+  icons.map(icon => Object.assign({}, icon, parseSVG(icon.svg)))
 
 const build = () => {
-  const icons = readIcons(src)
-  const data = getData(icons)
-    .reduce((a, { key, svg, viewBox, path }) => Object.assign({}, a, {
-      [key]: {
-        viewBox,
-        path
-      }
-    }), {})
+  const legacyIcons = readIcons(legacySrc)
+  const newIcons = readIcons(newSrc)
+  const icons = [...legacyIcons, ...newIcons]
+  const data = getData(icons).reduce(
+    (a, { key, svg, viewBox, path }) =>
+      Object.assign({}, a, {
+        [key]: {
+          viewBox,
+          path
+        }
+      }),
+    {}
+  )
   writeFile(data)
 }
 
