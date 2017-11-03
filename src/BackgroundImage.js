c51e408bd6b47fe866ca3e41346b034fa4de62d6
daljeet kukreja <daljeet.kukreja@priceline.com>
2017-11-03 12:41:42 -0400

    width for background image

--- src/BackgroundImage.js
+++ src/BackgroundImage.js
@@ -1,26 +1,29 @@
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import { width, responsiveStyle } from 'styled-system'
 
-const image = props => props.image
-  ? { backgroundImage: `url(${props.image})` }
-  : null
+const image = props =>
+  props.image ? { backgroundImage: `url(${props.image})` } : null
 
-const height = props => props.height
-  ? { height: props.height }
-  : null
+const height = props => (props.height ? { height: props.height } : null)
 
 const BackgroundImage = styled.div`
-  ${image}
-  ${height}
-  background-color: ${props => props.theme.colors.gray};
+  ${image} ${height} ${width} background-color: ${props =>
+      props.theme.colors.gray};
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
 `
+const numberStringOrArray = PropTypes.oneOfType([
+  PropTypes.number,
+  PropTypes.string,
+  PropTypes.array
+])
 
 BackgroundImage.propTypes = {
   /** background-image url */
-  image: PropTypes.string
+  image: PropTypes.string,
+  width: numberStringOrArray
 }
 
 export default BackgroundImage
