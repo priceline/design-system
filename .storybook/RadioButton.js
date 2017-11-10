f776b93dda886f69200e15f4d89aca769336a21d
tornglintaffychen <tornglinchen@gmail.com>
2017-11-10 15:26:41 -0500

    #68: update storybook to just have one story showing 3 states

--- .storybook/RadioButton.js
+++ .storybook/RadioButton.js
@@ -2,24 +2,27 @@ import React from 'react'
 import { storiesOf, action } from '@storybook/react'
 import { RadioButton } from '../src'
 
-storiesOf('RadioButton', module)
-  .add('selected', () => (
-    <RadioButton
-      onClick={action('clicked')}
-      isSelected
-      radioButtonText="Option Selected"
-    />
-  ))
-  .add('not selected', () => (
-    <RadioButton
-      onClick={action('clicked')}
-      radioButtonText="Option Not Selected"
-    />
-  ))
-  .add('disabled, click does not do anything', () => (
-    <RadioButton
-      onClick={action('clicked')}
-      disabled
-      radioButtonText="Option Disabled"
-    />
-  ))
+storiesOf('RadioButton', module).add('3 states', () => (
+  <div>
+    <di>
+      <RadioButton
+        onClick={action('clicked')}
+        isSelected
+        radioButtonText="Option Selected"
+      />
+    </di>
+    <div>
+      <RadioButton
+        onClick={action('clicked')}
+        radioButtonText="Option Not Selected"
+      />
+    </div>
+    <div>
+      <RadioButton
+        onClick={action('clicked')}
+        disabled
+        radioButtonText="Option Disabled"
+      />
+    </div>
+  </div>
+))
