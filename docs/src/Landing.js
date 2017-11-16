f82692f30791d7d2545c9c00765eaaa2cae2ed45
Brent Jackson <jxnblk@gmail.com>
2017-11-16 09:50:26 -0500

    Adjust landing page styles for docs

--- docs/src/Landing.js
+++ docs/src/Landing.js
@@ -16,35 +16,44 @@ import Markdown from './Markdown'
 
 const Btn = Button.withComponent('a')
 
+const image = `https://images.unsplash.com/photo-1508522109214-4df07c2e9d90?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=ad8b7153fe7b431953e6d22c3d869960`
+
 const Landing = props => {
   const gettingStarted = props.content.find(c => c.name === 'GettingStarted')
     .content
   return (
-    <Box p={[2, 4]}>
+    <Box>
       <BackgroundImage
-        height="40vh"
-        image="http://source.unsplash.com/featured/1600x900/?travel"
+        height="50vh"
+        image={image}
+        style={
+          {
+            // backgroundPosition: 'bottom center',
+            // backgroundAttachment: 'fixed'
+          }
+        }
       />
-      <Container maxWidth={768}>
-        <PageTitle>Priceline One</PageTitle>
-        <Text fontSize={3} mb={2}>
-          {props.pkg.description}
-        </Text>
-        <Text fontSize={0}>v{props.pkg && props.pkg.version}</Text>
-        <Divider my={4} />
-        <Flex wrap mx={-2} align="center">
-          <Pre m={2}>npm install pcln-design-system</Pre>
-          <Btn
-            m={2}
-            href="https://github.com/pricelinelabs/design-system-sketch/releases/latest"
-            children="Sketch Download"
-          />
-        </Flex>
-        <Box py={5}>
-          <Markdown children={gettingStarted} />
-        </Box>
-      </Container>
-      {false && <pre children={JSON.stringify(props, null, 2)} />}
+      <Box p={[2, 4]}>
+        <Container maxWidth={768}>
+          <PageTitle>Priceline One</PageTitle>
+          <Text fontSize={3} mb={2}>
+            {props.pkg.description}
+          </Text>
+          <Text fontSize={0}>v{props.pkg && props.pkg.version}</Text>
+          <Divider my={4} />
+          <Flex wrap mx={-2} align="center">
+            <Pre m={2}>npm install pcln-design-system</Pre>
+            <Btn
+              m={2}
+              href="https://github.com/pricelinelabs/design-system-sketch/releases/latest"
+              children="Sketch Download"
+            />
+          </Flex>
+          <Box py={5}>
+            <Markdown children={gettingStarted} />
+          </Box>
+        </Container>
+      </Box>
     </Box>
   )
 }
