56917ba60fabb23e0f5c2af2fe6a1577d578bd4b
Oliver Dumoulin <hello@oliver.design>
2018-01-11 16:31:21 -0500

    cleanup

--- .storybook/Tag.js
+++ .storybook/Tag.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
-import { Tag } from '../src'
-import { Icon } from '../src'
+import { Tag, Icon } from '../src'
 
 storiesOf('Tag', module)
   .add(
