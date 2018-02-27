a67415f623b77782188331766e0193b36766a23b
Oliver Dumoulin <hello@oliver.design>
2018-02-27 14:48:29 -0500

    fixed tests

--- src/Stamp.js
+++ src/Stamp.js
@@ -15,8 +15,7 @@ const Stamp = styled.div`
   border-radius: 2px;
   border-width: 1px;
   border-style: solid;
-  border-color: ${props =>
-    props.disabled ? null : props.theme.colors.borderGray};
+  border-color: ${props => theme.colors.borderGray};
   ${space} ${fontSize} ${color};
 `
 
