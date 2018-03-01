1a1500be5879c821e2e2525c9633376a9f7e87e7
Brent Jackson <jxnblk@gmail.com>
2018-03-01 11:38:42 -0500

    Update dependencies and move styled-components to peerDependencies

--- test-setup.js
+++ test-setup.js
@@ -1,5 +1,5 @@
 import 'jest-styled-components'
 import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-15'
+import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
