87383cf21b8771840e240390085a19069b6cc627
Brent Jackson <jxnblk@gmail.com>
2018-03-01 16:25:02 -0500

    Update Text component props API

--- src/Text.js
+++ src/Text.js
@@ -1,6 +1,13 @@
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import { fontSize, space, color, responsiveStyle } from 'styled-system'
+import {
+  fontSize,
+  fontWeight,
+  textAlign,
+  space,
+  color,
+  propTypes
+} from 'styled-system'
 import theme from './theme'
 
 export const caps = props =>
@@ -16,50 +23,28 @@ export const regular = props =>
 export const bold = props =>
   props.bold ? { fontWeight: props.theme.bold } : null
 
-export const italic = props => (props.italic ? { fontStyle: 'italic' } : null)
-
-const align = responsiveStyle('text-align', 'align')
-
 const Text = styled.div`
-  ${italic} ${fontSize} ${space} ${color} ${caps} ${regular} ${bold} ${align};
+  ${fontSize}
+  ${fontWeight}
+  ${textAlign}
+  ${space}
+  ${color}
+  ${caps}
+  ${regular}
+  ${bold}
 `
 
 Text.displayName = 'Text'
 
-const numberStringOrArray = PropTypes.oneOfType([
-  PropTypes.number,
-  PropTypes.string,
-  PropTypes.array
-])
-
 Text.propTypes = {
-  fontSize: PropTypes.oneOfType([
-    PropTypes.number,
-    PropTypes.string,
-    PropTypes.array
-  ]),
-  align: PropTypes.oneOf(['left', 'center', 'right', 'justify']),
+  ...propTypes.fontSize,
+  ...propTypes.fontWeight,
+  ...propTypes.textAlign,
+  ...propTypes.space,
+  ...propTypes.color,
   caps: PropTypes.bool,
   regular: PropTypes.bool,
-  bold: PropTypes.bool,
-  italic: PropTypes.bool,
-  color: PropTypes.string,
-  /** Margin */
-  m: numberStringOrArray,
-  mt: numberStringOrArray,
-  mr: numberStringOrArray,
-  mb: numberStringOrArray,
-  ml: numberStringOrArray,
-  mx: numberStringOrArray,
-  my: numberStringOrArray,
-  /** Padding */
-  p: numberStringOrArray,
-  pt: numberStringOrArray,
-  pr: numberStringOrArray,
-  pb: numberStringOrArray,
-  pl: numberStringOrArray,
-  px: numberStringOrArray,
-  py: numberStringOrArray
+  bold: PropTypes.bool
 }
 
 Text.defaultProps = {
