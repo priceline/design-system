aa0a23ff7265a1357d47bfa2aade172dcf8d8f8e
sqott <scott@scottgary.com>
2018-04-23 15:05:17 -0400

    Syntax updates

--- src/theme.js
+++ src/theme.js
@@ -152,9 +152,9 @@ export const duration = {
 }
 
 // animation easing curves
-const easeInOut = `cubic-bezier(0.5, 0, 0.25, 1)`
-const easeOut = `cubic-bezier(0, 0, 0.25, 1)`
-const easeIn = `cubic-bezier(0.5, 0, 1, 1)`
+const easeInOut = 'cubic-bezier(0.5, 0, 0.25, 1)'
+const easeOut = 'cubic-bezier(0, 0, 0.25, 1)'
+const easeIn = 'cubic-bezier(0.5, 0, 1, 1)'
 
 const easing = {
   easeInOut,
