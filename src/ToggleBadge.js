1629cabc742c8b295af4f25f9f9d74a732f451df
snigam <snigam@priceline.com>
2018-06-26 15:34:10 -0400

    making it a button instead of Button

--- src/ToggleBadge.js
+++ src/ToggleBadge.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { theme as themeGet, space, fontSize, propTypes } from 'styled-system'
 import theme from './theme'
-import Button from './Button'
 
-const ToggleBadge = styled(Button)`
+const ToggleBadge = styled.button`
   border-radius: ${props => props.theme.radius};
+  border: 0;
   display: inline-block;
   font-weight: ${props => props.theme.bold};
+  font-family: inherit;
+  cursor: pointer;
   background-color: ${props =>
     props.selected ? props.theme.colors[props.bg] : props.theme.colors.white};
   color: ${props => props.theme.colors[props.color]};
