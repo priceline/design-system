408ed97c5dd7c5461561fad9e6f9b8beafc95756
Brent Jackson <jxnblk@gmail.com>
2018-09-12 15:41:14 -0400

    Remove comments

--- src/__tests__/FormField.js
+++ src/__tests__/FormField.js
@@ -80,29 +80,6 @@ describe('FormField', () => {
     expect(label.props.style.opacity).toBe(1)
   })
 
-  /*
-  test('it correctly places an aria-label with the placeholder value before user has interacted with the input', () => {
-    const test = mount(
-      <FormField>
-        <Label>A Label</Label>
-        <Icon name="email" />
-        <Input id="with-both-icons" placeholder="placeholder text" />
-        <Icon name="email" />
-      </FormField>
-    )
-
-    // before user interaction the Label element should not be rendered
-    let label = test.find(Label)
-    expect(label.length).toBe(0)
-
-    let input = test.find(Input)
-    expect(input.getDOMNode().getAttribute('aria-label')).toBe(
-      'placeholder text'
-    )
-  })
-
-  */
-
   test('warns when there is no form field', () => {
     const spy = jest.spyOn(global.console, 'error')
     renderIntoDocument(
@@ -124,70 +101,4 @@ describe('FormField', () => {
     expect(spy).toHaveBeenCalledTimes(1)
     spy.mockRestore()
   })
-
-  /*
-  test('it triggers a prop-type warning when any element besides Label, Icon, and Input is provided as a child.', () => {
-    console.error = jest.fn()
-
-    const test = mount(
-      <FormField onChange={() => {}}>
-        <span>Extra stuff not supported</span>
-        <Label>A Label</Label>
-
-        <Icon name="email" />
-        <Icon name="email" />
-        <Input id="with-both-icons" />
-        <Icon name="email" />
-      </FormField>
-    )
-
-    expect(
-      console.error.mock.calls
-        .toString()
-        .indexOf('is not a valid child for') !== -1
-    )
-    console.error.mockRestore()
-  })
-  test('it triggers a prop-type warning when more than one Label is provided', () => {
-    console.error = jest.fn()
-
-    const test = mount(
-      <FormField onChange={() => {}}>
-        <Label>A Label</Label>
-        <Label>Another Label</Label>
-        <Input id="with-both-icons" />
-      </FormField>
-    )
-
-    expect(
-      console.error.mock.calls
-        .toString()
-        .indexOf("Exactly 0 or 1 'Label' children should be supplied to ") !==
-        -1
-    )
-    console.error.mockRestore()
-  })
-  test('it triggers a prop-type warning when there is more than one icon before or after the Input element', () => {
-    console.error = jest.fn()
-
-    const test = mount(
-      <FormField onChange={() => {}}>
-        <span>Extra stuff not supported</span>
-        <Label>A Label</Label>
-
-        <Icon name="email" />
-        <Icon name="email" />
-        <Input id="with-both-icons" />
-      </FormField>
-    )
-
-    expect(
-      console.error.mock.calls
-        .toString()
-        .indexOf('component must be positioned between them as children of') !==
-        -1
-    )
-    console.error.mockRestore()
-  })
-  */
 })
