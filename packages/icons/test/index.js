906bd934a5c907337c6f86d1cb0514bfd3d5db02
Brent Jackson <jxnblk@gmail.com>
2018-09-13 13:21:53 -0400

    Add wrapper Icon component

--- packages/icons/test/index.js
+++ packages/icons/test/index.js
@@ -3,6 +3,9 @@ import TestRenderer from 'react-test-renderer'
 import 'jest-styled-components'
 import * as customIcons from '../src/custom'
 import * as mdIcons from '../src/material-design'
+import Icon from '../src/Icon'
+import AirplaneIcon from '../src/custom/Airplane'
+import AcUnitIcon from '../src/material-design/AcUnit'
 
 const icons = [
   ...Object.keys(customIcons).map(key => [key, customIcons[key]]),
@@ -12,4 +15,18 @@ const icons = [
 test.each(icons)('renders %s', (key, Component) => {
   const json = TestRenderer.create(<Component />).toJSON()
   expect(json).toMatchSnapshot()
+})
+
+describe('Icon', () => {
+  test('renders custom icon', () => {
+    const expected = TestRenderer.create(<AirplaneIcon />).toJSON()
+    const json = TestRenderer.create(<Icon name="Airplane" />).toJSON()
+    expect(json).toEqual(expected)
+  })
+
+  test('renders material design icon', () => {
+    const expected = TestRenderer.create(<AcUnitIcon />).toJSON()
+    const json = TestRenderer.create(<Icon name="AcUnit" />).toJSON()
+    expect(json).toEqual(expected)
+  })
 })
