d0a8ca84e5be8c9e0a67bc0d4da6c279661526c0
Brent Jackson <jxnblk@gmail.com>
2018-10-01 13:45:00 -0400

    Adjust prepare scripts and add icon aliases

--- packages/icons/src/Icon.js
+++ packages/icons/src/Icon.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import upperFirst from 'lodash.upperfirst'
 import * as icons from './icons'
 
+const aliases = {
+  ac: 'AcUnit'
+}
+
 const Icon = ({ name, ...props }) => {
-  const Component = icons[name]
+  const key = upperFirst(name)
+  const Component = icons[name] || icons[key] || icons[aliases[name]]
   if (!Component) return false
   return <Component {...props} />
 }
