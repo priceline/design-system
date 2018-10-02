fc3b90f804bb1c956db008faca12f03fe06a72d9
Brent Jackson <jxnblk@gmail.com>
2018-10-02 16:59:02 -0400

    Fix storybook setup

--- packages/core/storybook/Checkbox.js
+++ packages/core/storybook/Checkbox.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-import { storiesOf, action } from '@storybook/react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 import {
   Checkbox,
   Text,
