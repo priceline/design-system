be2720620c02969a2a52dc440bf5f0f7515db36d
Beni von Cheni <benjaminlchen@gmail.com>
2018-11-05 23:26:16 -0500

    Use babel-plugin-module-resolver for pcln-icons

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,14 @@ module.exports = {
   plugins: [
     '@babel/transform-runtime',
     '@babel/proposal-class-properties',
-    '@babel/proposal-object-rest-spread'
+    '@babel/proposal-object-rest-spread',
+    [
+      'module-resolver',
+      {
+        alias: {
+          'pcln-icons': './packages/icons'
+        }
+      }
+    ]
   ]
 }
