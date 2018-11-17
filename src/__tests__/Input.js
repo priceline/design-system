7b4dd206e1416896c156e3b5a86456dd9627dbb0
Steve Dalonzo <steven.dalonzo@priceline.com>
2018-11-17 18:16:27 -0500

    fix warning about missing id in Input tests
    added fake test id

--- src/__tests__/Input.js
+++ src/__tests__/Input.js
@@ -3,21 +3,23 @@ import renderer from 'react-test-renderer'
 import 'jest-styled-components'
 import { Input } from '..'
 
+const id = 'fake-test-id'
+
 describe('Input', () => {
   test('it renders', () => {
-    const json = renderer.create(<Input />).toJSON()
+    const json = renderer.create(<Input id={id} />).toJSON()
     expect(json).toMatchSnapshot()
   })
   test('it renders an input element with a red border with a color prop is passed', () => {
-    const json = renderer.create(<Input color="red" />).toJSON()
+    const json = renderer.create(<Input id={id} color="red" />).toJSON()
     expect(json).toMatchSnapshot()
   })
   test('it renders an input element with a really large padding and margin', () => {
-    const json = renderer.create(<Input p={4} m={4} />).toJSON()
+    const json = renderer.create(<Input id={id} p={4} m={4} />).toJSON()
     expect(json).toMatchSnapshot()
   })
   test('it renders an input element with large text', () => {
-    const json = renderer.create(<Input fontSize={4} />).toJSON()
+    const json = renderer.create(<Input id={id} fontSize={4} />).toJSON()
     expect(json).toMatchSnapshot()
   })
 })
