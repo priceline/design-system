21500d34717f730f61bb2116f033a1745950b70b
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2018-11-19 15:59:33 -0500

    Rename AppBar story

--- .storybook/AppBar.js
+++ .storybook/AppBar.js
@@ -4,7 +4,7 @@ import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 import { Box, AppBar } from '../src'
 import theme from '../src/theme'
 
-storiesOf('Navigation', module).add('AppBar', () => (
+storiesOf('AppBar', module).add('AppBar component', () => (
   <Box bg={theme.colors.gray100} p={2}>
     <AppBar
       inverse={boolean('Inverse', false)}   
