3782931da5859351e14facc035a95011bf9c41a7
Beni von Cheni <benjaminlchen@gmail.com>
2019-01-06 23:45:00 -0500

    Update rollup configs for core & slider packages

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,4 +1,3 @@
 const rollupConfig = require('../../rollup.config')
-rollupConfig.external.push('pcln-icons')
 rollupConfig.external.push('pcln-slider')
 module.exports = rollupConfig
--- packages/slider/rollup.config.js
+++ packages/slider/rollup.config.js
@@ -1,4 +1,3 @@
 const rollupConfig = require('../../rollup.config')
 rollupConfig.external.push('pcln-design-system')
-rollupConfig.external.push('pcln-icons')
 module.exports = rollupConfig
