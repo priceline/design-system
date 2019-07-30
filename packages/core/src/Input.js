ff725d98aaa770497e6f0dd73faa6fc42fdc66e7
Kyle Pollock <kyle.pollock@priceline.com>
2019-07-30 16:24:08 -0500

    Remove unused keyframes import

--- packages/core/src/Input.js
+++ packages/core/src/Input.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
 import { space, themeGet } from 'styled-system'
 import PropTypes from 'prop-types'
 import defaultTheme from './theme'
