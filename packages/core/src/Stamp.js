42676fc74864061f1603b794b32f9dc45b4433c0
Adam Choboter <achoboter@shaw.ca>
2019-08-06 10:54:08 -0400

    feat(theming): adding theming support
    -merge master

--- packages/core/src/Stamp.js
+++ packages/core/src/Stamp.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { themeGet, space, fontSize } from 'styled-system'
 import { getPaletteColor, deprecatedColorValue } from './utils'
