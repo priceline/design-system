e7d26175611c2f61d1c8fddc1c7ee4447feb296e
Adam Choboter <achoboter@shaw.ca>
2019-08-21 13:35:14 -0400

    feat(theming): adding theming support
    -fixing Checkbox tests after merging master

--- packages/core/test/Checkbox.js
+++ packages/core/test/Checkbox.js
@@ -7,26 +7,30 @@ afterEach(cleanup)
 describe('Checkbox', () => {
   test('renders without the theme passed specifically', () => {
     const onChange = jest.fn()
-    const { asFragment } = render(
+    const { asFragment } = renderWithTheme(
       <Checkbox id="check-box" onChange={onChange} />
     )
     expect(asFragment()).toMatchSnapshot()
   })
 
   test('renders with the theme passed specifically', () => {
-    const { asFragment } = render(<Checkbox id="check-box" theme={theme} />)
+    const { asFragment } = renderWithTheme(
+      <Checkbox id="check-box" theme={theme} />
+    )
     expect(asFragment()).toMatchSnapshot()
   })
 
   test('renders checked when defaultChecked prop is passed as true', () => {
-    const { getByRole } = render(<Checkbox id="check-box" defaultChecked />)
+    const { getByRole } = renderWithTheme(
+      <Checkbox id="check-box" defaultChecked />
+    )
     const checkbox = getByRole('checkbox')
     expect(checkbox.checked).toBe(true)
   })
 
   test('renders disabled with disabled prop', () => {
     const onChange = jest.fn()
-    const { asFragment } = render(
+    const { asFragment } = renderWithTheme(
       <Checkbox id="check-box" disabled={true} onChange={onChange} />
     )
     expect(asFragment()).toMatchSnapshot()
