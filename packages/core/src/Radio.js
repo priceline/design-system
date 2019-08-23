755128a21797b892ef037ac3f1381c5e95f927ad
Craig Palermo <unscsprt@users.noreply.github.com>
2019-08-23 14:47:09 +0000

    feat: Update focus and hover states for Radio component (#568)
    Authored by @unscsprt. Merged by @unscsprt.
    
    PR-URL: #568 
    Reviewed-By: @adhagam, @dgiraldo313

--- packages/core/src/Radio.js
+++ packages/core/src/Radio.js
@@ -1,59 +1,76 @@
-import React from 'react'
-import styled from 'styled-components'
-import Icon from './Icon'
-import { applyVariations, deprecatedColorValue, getPaletteColor } from './utils'
+import React from "react";
+import styled from "styled-components";
+import Icon from "./Icon";
+import {
+  applyVariations,
+  deprecatedColorValue,
+  getPaletteColor
+} from "./utils";
 
 const RadioWrap = styled.div`
   display: inline-block;
-  color: ${getPaletteColor('border.base')};
-  &:hover {
-    ${props =>
-      props.checked || props.disabled
-        ? null
-        : `color: ${getPaletteColor('base')(props)};`};
+  color: ${props => props.theme.colors.borderGray};
+  &:hover > svg {
+    ${props => {
+      if (props.checked && !props.disabled) {
+        return `color: ${props.theme.colors.darkBlue} !important;`;
+      }
+
+      return props.disabled ? null : `color: ${props.theme.colors.blue};`;
+    }};
   }
-  ${applyVariations('Radio')}
-`
+  ${applyVariations("Radio")}
+`;
 
 const RadioInput = styled.input`
   appearance: none;
   opacity: 0;
   position: absolute;
   z-index: 0;
+
+  & ~ svg {
+    border: 1px solid transparent;
+    border-radius: 50%;
+    padding: 2px;
+  }
+
   &:focus {
     box-shadow: none;
+    & ~ svg {
+      border: 1px solid ${theme.colors.borderGray};
+      background-color: ${theme.colors.lightGray};
+    }
   }
   &:checked ~ svg {
-    color: ${getPaletteColor('base')};
+    color: ${getPaletteColor("base")};
   }
   &:disabled ~ svg {
-    color: ${getPaletteColor('border.base')};
+    color: ${getPaletteColor("border.base")};
   }
-`
+`;
 
 const RadioIcon = styled(Icon)`
   vertical-align: middle;
-`
+`;
 
 const Radio = props => {
-  const { checked, disabled } = props
+  const { checked, disabled, size } = props;
 
-  const radioIconName = checked ? 'radioChecked' : 'radioEmpty'
+  const radioIconName = checked ? "radioChecked" : "radioEmpty";
+
+  const borderAdjustedSize = size + 4;
 
   return (
     <RadioWrap color={props.color} checked={checked} disabled={disabled}>
       <RadioInput type="radio" {...props} />
-      <RadioIcon name={radioIconName} size={24} />
+      <RadioIcon name={radioIconName} size={borderAdjustedSize} />
     </RadioWrap>
-  )
-}
+  );
+};
 
 Radio.defaultProps = {
-  color: 'primary'
-}
+  theme: theme,
+  size: 24
+};
 
-Radio.propTypes = {
-  color: deprecatedColorValue()
-}
-
-export default Radio
+export default Radio;
