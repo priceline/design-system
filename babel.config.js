51ad23f0fe2331497c12d8f7652e75ec717f3ccb
Bing Bai <bert_b@live.com>
2019-08-23 10:50:06 -0400

    fix: modal style chaining
    Authored by @bertya . Merged by @BeniCheni.
    
    PR-URL: #563 
    Reviewed-By: @unscsprt, @BeniCheni

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ module.exports = {
   plugins: [
     '@babel/transform-runtime',
     '@babel/proposal-class-properties',
-    '@babel/proposal-object-rest-spread'
+    '@babel/proposal-object-rest-spread',
+    '@babel/plugin-proposal-optional-chaining'
   ]
 }
