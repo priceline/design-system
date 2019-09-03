4583aa92e1ef7ce3d714a9f10a15c1317c1bc9fe
James Stewart <james.stewart@priceline.com>
2019-09-03 12:44:58 -0400

    Remove Warnings and Update Readme for Theming

--- packages/icons/src/Icon.js
+++ packages/icons/src/Icon.js
@@ -1,55 +1,39 @@
 import React from 'react'
 import upperFirst from 'lodash.upperfirst'
 import PropTypes from 'prop-types'
-
 import * as icons from './index'
 
 const Icon = ({ name, title, desc, titleId, descId, ...props }) => {
   const Component = icons[name] || icons[upperFirst(name)]
-  if (!Component) return false
 
-  /**
-   *  To support older browsers, make sure 'titleId' is passed along with 'title' props and
-   * 'descId' with 'desc' props
-   * Also, <desc> element should be followed by a <title> tag for <svg> elements
-   * */
-  if (!!title) {
-    let ariaLabelledBy = ''
-    props['aria-hidden'] = 'false'
-
-    if (!!titleId) {
-      ariaLabelledBy = titleId
-    }
-
-    if (!!desc && !!descId) {
-      ariaLabelledBy = `${ariaLabelledBy} ${descId}`
-    }
-
-    if (!!ariaLabelledBy) {
-      props['aria-labelledby'] = ariaLabelledBy
-    }
-  }
+  let ariaLabelledBy = titleId ? titleId : ''
+  ariaLabelledBy += desc && descId ? ` ${descId}` : ''
+  ariaLabelledBy = ariaLabelledBy ? ariaLabelledBy : undefined
 
   return (
-    <Component
-      title={title}
-      desc={desc}
-      titleId={titleId}
-      descId={descId}
-      {...props}
-    />
+    Component && (
+      <Component
+        title={title}
+        desc={desc}
+        titleId={titleId}
+        descId={descId}
+        aria-hidden={!!ariaLabelledBy}
+        aria-labelledby={ariaLabelledBy}
+        {...props}
+      />
+    )
   )
 }
 
+Icon.isIcon = true
+Icon.displayName = 'Icon'
+
 Icon.defaultProps = {
   size: 24,
-  'aria-hidden': 'true',
-  focusable: 'false',
-  tabIndex: '-1'
+  tabIndex: -1,
+  focusable: false,
+  'aria-hidden': true
 }
-
-Icon.displayName = 'Icon'
-Icon.isIcon = true
 
 Icon.propTypes = {
   name: (props, key, componentName) => {
@@ -58,40 +42,11 @@ Icon.propTypes = {
       return new Error(
         `Unknown name prop \`${name}\` supplied to \`${componentName}\``
       )
-    } else if (!icons[name]) {
-      return new Error(
-        `Icon name prop should be uppercase.\n` +
-          `Use \`${upperFirst(name)}\` instead of ${name}.`
-      )
     }
   },
-  title: (props, propName, componentName) => {
-    if (typeof props[propName] !== 'string') {
-      return new Error(
-        `'title' prop supplied to '${componentName}' should be a string`
-      )
-    } else if (!!props[propName] && !props['titleId']) {
-      return new Error(
-        `'titleId' prop should be passed along with 'title' prop to '${componentName}'`
-      )
-    }
-  },
-  desc: (props, propName, componentName) => {
-    if (typeof props[propName] !== 'string') {
-      return new Error(
-        `'desc' prop supplied to '${componentName}' should be a string`
-      )
-    } else if (!!props[propName] && !props['title']) {
-      return new Error(
-        `'title' prop should be passed along with 'desc' prop to '${componentName}'`
-      )
-    } else if (!!props[propName] && !props['descId']) {
-      return new Error(
-        `'descId' prop should be passed along with 'desc' prop to '${componentName}'`
-      )
-    }
-  },
+  title: PropTypes.string,
   titleId: PropTypes.string,
+  desc: PropTypes.string,
   descId: PropTypes.string
 }
 
