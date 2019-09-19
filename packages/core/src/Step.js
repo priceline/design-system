ce7bfff2c4f0eedf626e2721b3327572df22b2f0
James Stewart <james.stewart@priceline.com>
2019-09-19 16:03:46 -0400

    Ally updated

--- packages/core/src/Step.js
+++ packages/core/src/Step.js
@@ -1,21 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import Flex from './Flex'
+import Button from './Button'
 import Text from './Text'
 import CheckIcon from 'pcln-icons/lib/Check'
+import { getPaletteColor } from './utils'
 
-const StyledFlex = styled(Flex)`
+const StyledFlex = styled(Button)`
+  display: flex;
+  background-color: ${getPaletteColor('background.lightest')};
+  color: ${getPaletteColor('base')};
   cursor: ${props => (props.onClick ? 'pointer' : 'default')};
+  padding: 0;
+  font-weight: unset;
+
+  &:hover {
+    background-color: ${getPaletteColor('background.lightest')};
+  }
 `
 
 function Step({ className, active, completed, children, onClick }) {
   const color = active || completed ? 'primary' : 'text.light'
 
   return (
-    <StyledFlex className={className} alignItems="center" onClick={onClick}>
+    <StyledFlex
+      className={className}
+      disabled={!onClick}
+      alignItems="center"
+      aria-selected={active}
+      onClick={onClick}
+    >
       {completed && (
-        <CheckIcon color="primary" size={16} mr={1} legacy={false} />
+        <CheckIcon
+          color="primary"
+          size={16}
+          mr={1}
+          legacy={false}
+          aria-label="Step Completed"
+        />
       )}
       <Text color={color} bold={active} fontSize={1}>
         {children}
