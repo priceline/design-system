6eeec402ca22234dd3ec99bc6b75493f565ac567
Dalonzo, Steven <Steven.Dalonzo@priceline.com>
2019-09-30 14:07:52 -0400

    bugfix poc

--- packages/core/src/Stepper.js
+++ packages/core/src/Stepper.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { themeGet } from 'styled-system'
+// import { themeGet } from 'styled-system'
 import Flex from './Flex'
 import Step from './Step'
 
 const StyledFlex = styled(Flex)`
-  & > :not(:last-child) {
+  /* & > :not(:last-child) {
     margin-right: ${themeGet('space.4')}px;
-  }
+  } */
 `
 
 function Stepper({ className, children }) {
