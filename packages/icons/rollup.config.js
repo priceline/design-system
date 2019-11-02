920debbb53e1dabe7e94a824a6a49d8a9de9f354
Dalonzo, Steven <Steven.Dalonzo@priceline.com>
2019-11-02 01:23:23 -0400

    wip rollup icons

--- packages/icons/rollup.config.js
+++ packages/icons/rollup.config.js
@@ -1,10 +1,12 @@
-const svgr = require('@svgr/rollup')
+// const svgr = require('@svgr/rollup')
 
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/app.js',
-    format: 'iife'
-  },
-  plugins: [svgr()]
-}
+// export default {
+//   input: 'src/index.js',
+//   output: {
+//     file: 'dist/app.js',
+//     format: 'iife'
+//   },
+//   plugins: [svgr()]
+// }
+
+module.exports = require('../../rollup.config')
