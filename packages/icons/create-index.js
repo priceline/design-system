ce9c479decb618705b4a2d7674153471c6b2af69
Dalonzo, Steven <Steven.Dalonzo@priceline.com>
2019-11-03 01:12:46 -0500

    wip

--- packages/icons/create-index.js
+++ packages/icons/create-index.js
@@ -8,8 +8,10 @@ const icons = fs
   .map(filename => path.basename(filename, '.js'))
 
 const template = icons => {
-  icons.map(name => `export { default as ${name} } from './${name}'`).join('\n')
-  const output = `export { default as Icon } from './Icon'\n${icons}`
+  icons.map(name => `export { default as ${name} } from './${name}'`)
+  const lines = [`export { default as Icon } from './Icon'`, ...icons]
+  const output = lines.join('\n')
+
   return output
 }
 
