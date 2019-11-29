35c8ac96dd4a2842c4d149969ef138aca176035c
Brianna Mansell <bjmansell@gmail.com>
2019-11-29 15:52:51 -0600

    Link rel when target is blank
    - Added conditional `rel` value when `target='_blank'`
    - Added new test to assert value is added
    - Update snapshots

--- packages/core/test/Link.js
+++ packages/core/test/Link.js
@@ -6,4 +6,21 @@ describe('Link', () => {
     const json = rendererCreateWithTheme(<Link>Dummy</Link>).toJSON()
     expect(json).toMatchSnapshot()
   })
+
+  test('adds a safe rel when target is blank', () => {
+    const json = rendererCreateWithTheme(
+      <Link target="_blank">Dummy</Link>
+    ).toJSON()
+
+    expect(json).toMatchSnapshot()
+    expect(json.props.target).toEqual('_blank')
+    expect(json.props.rel).toEqual('noopener')
+  })
+
+  test('does not add a safe rel when target is not blank', () => {
+    const json = rendererCreateWithTheme(<Link>Dummy</Link>).toJSON()
+
+    expect(json).toMatchSnapshot()
+    expect(json.props.rel).toBe(null)
+  })
 })
