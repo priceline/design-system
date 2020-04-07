6789b6421c0fe2c16a7cfdb1b9df4514f7d6bd0c
James Stewart <jamesstewart94.300@gmail.com>
2020-04-07 15:40:37 -0500

    Remove Unused Variable

--- packages/slider/src/styleSlider.js
+++ packages/slider/src/styleSlider.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { space, themeGet } from 'styled-system'
+import { space } from 'styled-system'
 import { getPaletteColor } from 'pcln-design-system'
 
 const styleSlider = Component => styled(Component)`
