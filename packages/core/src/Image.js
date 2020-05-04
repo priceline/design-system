c94d83e63bcd6318f88689d5770cf228590f32a9
James300 <jamesstewart94.300@gmail.com>
2020-05-04 13:32:55 -0500

    Lazy Loading by Default

--- packages/core/src/Image.js
+++ packages/core/src/Image.js
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 import { width } from 'styled-system'
 
 const Image = styled.img.attrs(props => ({
-  height: props.height || 'auto'
+  height: props.height || 'auto',
+  loading: props.loading || 'lazy'
 }))`
   display: block;
   max-width: 100%;
