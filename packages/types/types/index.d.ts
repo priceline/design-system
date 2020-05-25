2caf6093da8ad10e6c9578e513f43b83b028ea4f
Craig Palermo <email@craigp.me>
2020-05-25 14:33:42 -0400

    Begin adding prop types for DS core components

--- packages/types/types/index.d.ts
+++ packages/types/types/index.d.ts
@@ -31,8 +31,56 @@ export interface Theme {
 }
 
 export interface ThemeArgs {
-  color?: string
   theme: Theme
+}
+
+/**
+ * Pieces of the Styled System API represented as interfaces
+ */
+export interface SpaceProps {
+  margin?: string
+  m?: string
+  marginTop?: string
+  mt?: string
+  // add rest of props
+}
+
+export interface ColorProps {
+  color?: string
+  bg?: string
+  backgroundColor?: string
+}
+
+export interface TypographyProps {
+  fontSize?: number | string | Array<number | string>
+  textAlign?: string | string[]
+  // add rest of props
+}
+
+export interface LayoutProps {
+  width?: number | string | Array<number | string>
+  display?: string | string[]
+  // add rest of props
+}
+
+/**
+ * Combining Styled System interfaces depending on which Styled System functions
+ * the component uses
+ */
+export interface BoxProps
+  extends LayoutProps,
+    TypographyProps,
+    ColorProps,
+    SpaceProps {}
+
+/**
+ * Extend primitive Design System components to create interfaces more complex
+ * components
+ */
+export interface CardProps extends BoxProps {
+  boxShadowSize?: string
+  borderRadius?: number | string
+  borderWidth?: number
 }
 
 //
@@ -40,11 +88,11 @@ export interface ThemeArgs {
 // ------------------------------------------------------------
 
 // TODO add remaining component type declarations
-export class Card extends React.Component<any, any> {}
 export class Text extends React.Component<any, any> {}
 export class Select extends React.Component<any, any> {}
 export class Label extends React.Component<any, any> {}
-export class Box extends React.Component<any, any> {}
+export class Card extends React.Component<CardProps, any> {}
+export class Box extends React.Component<BoxProps, any> {}
 
 //
 // pcln-design-system utils
---
+++ packages/types/types/test.tsx
@@ -0,0 +1,25 @@
+import { getContrastRatio, deprecatedPropType, borders, hasPaletteColor, Box, Card } from 'pcln-types'
+import theme from '../../core/src/theme.js'
+import * as React from "react";
+
+//
+// Design System Utility Functions
+// =============================================================================================
+
+getContrastRatio('a', 'b') // $ExpectType number
+
+deprecatedPropType('myProp') // $ExpectType Error | undefined
+
+// $ExpectType object
+borders({
+  theme
+})
+
+hasPaletteColor({theme}) // $ExpectType boolean
+
+//
+// Design System Core Components
+// =============================================================================================
+
+const myBox = <Box width={'1'} bg={'primary'} /> // $ExpectType Element
+const myCard = <Card width={'1'} bg={'primary'} borderRadius={'md'} /> // $ExpectType Element
