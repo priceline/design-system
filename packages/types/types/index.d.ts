32bca53d98c1b2b9ac8802daf9e7822dac4ff3d6
Craig Palermo <email@craigp.me>
2020-05-25 15:47:36 -0400

    Scaffold out remaining types that need to be defined

--- packages/types/types/index.d.ts
+++ packages/types/types/index.d.ts
@@ -63,6 +63,45 @@ export interface LayoutProps {
   // add rest of props
 }
 
+export interface FlexboxProps {
+  alignItems?: string
+  alignContent?: string
+  justifyItems?: string
+  justifyContent?: string
+  flexWrap?: string
+  flexDirection?: string
+  flex?: string
+  flexGrow?: string
+  flexShrink?: string
+  flexBasis?: string
+  justifySelf?: string
+  alignSelf?: string
+  order?: string
+}
+
+// Grid Layout
+// TODO
+
+export interface Background {
+  background?: string
+  backgroundImage?: string
+  backgroundSize?: string
+  backgroundPosition?: string
+  backgroundRepeat?: string
+}
+
+// Border
+// TODO
+
+// Position
+// TODO
+
+// Shadow
+// TODO
+
+// Variants
+// TODO
+
 /**
  * Combining Styled System interfaces depending on which Styled System functions
  * the component uses
@@ -87,19 +126,51 @@ export interface CardProps extends BoxProps {
 // pcln-design-system components
 // ------------------------------------------------------------
 
-// TODO add remaining component type declarations
-export class Text extends React.Component<any, any> {}
-export class Select extends React.Component<any, any> {}
-export class Label extends React.Component<any, any> {}
-export class Card extends React.Component<CardProps, any> {}
+export class Absolute extends React.Component<any, any> {}
+export class Avatar extends React.Component<any, any> {}
+export class BackgroundImage extends React.Component<any, any> {}
+export class Badge extends React.Component<any, any> {}
+export class Banner extends React.Component<any, any> {}
+export class BlockLink extends React.Component<any, any> {}
 export class Box extends React.Component<BoxProps, any> {}
+export class Breadcrumbs extends React.Component<any, any> {}
+export class Button extends React.Component<any, any> {}
+export class Card extends React.Component<CardProps, any> {}
+export class Checkbox extends React.Component<any, any> {}
+export class CloseButton extends React.Component<any, any> {}
+export class Container extends React.Component<any, any> {}
+export class Divider extends React.Component<any, any> {}
+export class Flag extends React.Component<any, any> {}
+export class Flex extends React.Component<any, any> {}
+export class FormField extends React.Component<any, any> {}
+export class Heading extends React.Component<any, any> {}
+export class Hide extends React.Component<any, any> {}
+export class Hug extends React.Component<any, any> {}
+export class Icon extends React.Component<any, any> {}
+export class IconButton extends React.Component<any, any> {}
+export class IconField extends React.Component<any, any> {}
+export class Image extends React.Component<any, any> {}
+export class Input extends React.Component<any, any> {}
+export class InputGroup extends React.Component<any, any> {}
+export class Label extends React.Component<any, any> {}
+export class Link extends React.Component<any, any> {}
+export class PlaceholderImage extends React.Component<any, any> {}
+export class Radio extends React.Component<any, any> {}
+export class RatingBadge extends React.Component<any, any> {}
+export class Relative extends React.Component<any, any> {}
+export class Select extends React.Component<any, any> {}
+export class Stamp extends React.Component<any, any> {}
+export class Stepper extends React.Component<any, any> {}
+export class Text extends React.Component<any, any> {}
+export class TextArea extends React.Component<any, any> {}
+export class ToggleBadge extends React.Component<any, any> {}
+export class Tooltip extends React.Component<any, any> {}
+export class Truncate extends React.Component<any, any> {}
 
 //
 // pcln-design-system utils
 // ------------------------------------------------------------
 export function getContrastRatio(colorA: string, colorB: string): number
 export function deprecatedPropType(replacement: string): Error | undefined
-
 export function borders(props: ThemeArgs): object
-
 export function hasPaletteColor(props: ThemeArgs): boolean
