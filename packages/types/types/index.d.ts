fd79f48d01b2bbcd2c8cc3d04f90f5f19cf4bad4
Craig Palermo <email@craigp.me>
2020-05-25 18:36:59 -0400

    Restore Text definition, use Styled System types

--- packages/types/types/index.d.ts
+++ packages/types/types/index.d.ts
@@ -1,106 +1,22 @@
 // TypeScript Version: 3.0
 // Project: pcln-design-system
 
+/// <reference types="@types/styled-system" />
+
 import * as React from 'react'
-
-//
-// Constants and objects
-// ------------------------------------------------------------
-export interface Theme {
-  breakpoints: string[]
-  mediaQueries: string[]
-  space: number[]
-  font: string
-  fontSizes: number[]
-  fontWeights: object
-  lineHeights: object
-  letterSpacings: object
-  regular: number
-  bold: number
-  textStyles: object
-  colors: object
-  colorStyles: object
-  radii: number[]
-  radius: string
-  boxShadows: string[]
-  textShadows: object
-  maxContainerWidth: string
-  duration: object
-  timingFunctions: object
-  transitionDelays: object
-}
-
-export interface ThemeArgs {
-  theme: Theme
-}
-
-/**
- * Pieces of the Styled System API represented as interfaces
- */
-export interface SpaceProps {
-  margin?: string
-  m?: string
-  marginTop?: string
-  mt?: string
-  // add rest of props
-}
-
-export interface ColorProps {
-  color?: string
-  bg?: string
-  backgroundColor?: string
-}
-
-export interface TypographyProps {
-  fontSize?: number | string | Array<number | string>
-  textAlign?: string | string[]
-  // add rest of props
-}
-
-export interface LayoutProps {
-  width?: number | string | Array<number | string>
-  display?: string | string[]
-  // add rest of props
-}
-
-export interface FlexboxProps {
-  alignItems?: string
-  alignContent?: string
-  justifyItems?: string
-  justifyContent?: string
-  flexWrap?: string
-  flexDirection?: string
-  flex?: string
-  flexGrow?: string
-  flexShrink?: string
-  flexBasis?: string
-  justifySelf?: string
-  alignSelf?: string
-  order?: string
-}
-
-// Grid Layout
-// TODO
-
-export interface Background {
-  background?: string
-  backgroundImage?: string
-  backgroundSize?: string
-  backgroundPosition?: string
-  backgroundRepeat?: string
-}
-
-// Border
-// TODO
-
-// Position
-// TODO
-
-// Shadow
-// TODO
-
-// Variants
-// TODO
+import {
+  LayoutProps,
+  ColorProps,
+  BackgroundColorProps,
+  SpaceProps,
+  BoxShadowProps,
+  BorderRadiusProps,
+  Theme,
+  BorderWidthProps,
+  MarginProps,
+  FontSizeProps,
+  TextAlignProps
+} from 'styled-system'
 
 /**
  * Combining Styled System interfaces depending on which Styled System functions
@@ -108,18 +24,33 @@ export interface Background {
  */
 export interface BoxProps
   extends LayoutProps,
-    TypographyProps,
     ColorProps,
+    BackgroundColorProps,
     SpaceProps {}
 
 /**
- * Extend primitive Design System components to create interfaces more complex
+ * Extend primitive Design System components to create interfaces for more complex
  * components
  */
-export interface CardProps extends BoxProps {
-  boxShadowSize?: string
-  borderRadius?: number | string
-  borderWidth?: number
+export interface CardProps
+  extends BoxProps,
+    BoxShadowProps,
+    BorderRadiusProps,
+    BorderWidthProps {}
+
+export interface TextProps
+  extends MarginProps,
+    ColorProps,
+    FontSizeProps,
+    TextAlignProps {
+  /** Sets font-weight: props.theme.regular */
+  regular?: boolean
+  /** Sets font-weight: props.theme.bold */
+  bold?: boolean
+  /** Sets styles for all-caps type treatments */
+  caps?: boolean
+  /** Sets styles for italic type treatments */
+  italic?: boolean
 }
 
 //
@@ -133,7 +64,9 @@ export class Badge extends React.Component<any, any> {}
 export class Banner extends React.Component<any, any> {}
 export class BlockLink extends React.Component<any, any> {}
 export class Box extends React.Component<BoxProps, any> {}
-export class Breadcrumbs extends React.Component<any, any> {}
+export class Breadcrumbs extends React.Component<any, any> {
+  static Link
+}
 export class Button extends React.Component<any, any> {}
 export class Card extends React.Component<CardProps, any> {}
 export class Checkbox extends React.Component<any, any> {}
@@ -143,26 +76,52 @@ export class Divider extends React.Component<any, any> {}
 export class Flag extends React.Component<any, any> {}
 export class Flex extends React.Component<any, any> {}
 export class FormField extends React.Component<any, any> {}
-export class Heading extends React.Component<any, any> {}
+export class Heading extends React.Component<any, any> {
+  static h1
+  static h2
+  static h3
+  static h4
+  static h5
+  static h6
+}
 export class Hide extends React.Component<any, any> {}
 export class Hug extends React.Component<any, any> {}
 export class Icon extends React.Component<any, any> {}
 export class IconButton extends React.Component<any, any> {}
 export class IconField extends React.Component<any, any> {}
 export class Image extends React.Component<any, any> {}
-export class Input extends React.Component<any, any> {}
+export class Input extends React.Component<any, any> {
+  static isField: boolean
+}
 export class InputGroup extends React.Component<any, any> {}
-export class Label extends React.Component<any, any> {}
+export class Label extends React.Component<any, any> {
+  static isLabel: boolean
+}
 export class Link extends React.Component<any, any> {}
 export class PlaceholderImage extends React.Component<any, any> {}
 export class Radio extends React.Component<any, any> {}
 export class RatingBadge extends React.Component<any, any> {}
 export class Relative extends React.Component<any, any> {}
-export class Select extends React.Component<any, any> {}
+export class Select extends React.Component<any, any> {
+  static isField: boolean
+}
 export class Stamp extends React.Component<any, any> {}
-export class Stepper extends React.Component<any, any> {}
-export class Text extends React.Component<any, any> {}
-export class TextArea extends React.Component<any, any> {}
+export class Stepper extends React.Component<any, any> {
+  static Step
+}
+export class SrOnly extends React.Component<any, any> {}
+export class Text extends React.Component<TextProps, any> {
+  /** Span element */
+  static span
+  /** Paragraph element */
+  static p
+  /** Strikethrough element */
+  static s
+}
+export class TextArea extends React.Component<any, any> {
+  static isField: boolean
+}
+export class ThemeProvider extends React.Component<any, any> {}
 export class ToggleBadge extends React.Component<any, any> {}
 export class Tooltip extends React.Component<any, any> {}
 export class Truncate extends React.Component<any, any> {}
@@ -170,6 +129,11 @@ export class Truncate extends React.Component<any, any> {}
 //
 // pcln-design-system utils
 // ------------------------------------------------------------
+
+export interface ThemeArgs {
+  theme: Theme
+}
+
 export function getContrastRatio(colorA: string, colorB: string): number
 export function deprecatedPropType(replacement: string): Error | undefined
 export function borders(props: ThemeArgs): object
--- packages/types/types/test.tsx
+++ packages/types/types/test.tsx
@@ -1,4 +1,4 @@
-import { getContrastRatio, deprecatedPropType, borders, hasPaletteColor, Box, Card } from 'pcln-types'
+import {getContrastRatio, deprecatedPropType, borders, hasPaletteColor, Box, Card, Text} from 'pcln-types'
 import theme from '../../core/src/theme.js'
 import * as React from "react";
 
@@ -23,3 +23,5 @@ hasPaletteColor({theme}) // $ExpectType boolean
 
 const myBox = <Box width={'1'} bg={'primary'} /> // $ExpectType Element
 const myCard = <Card width={'1'} bg={'primary'} borderRadius={'md'} /> // $ExpectType Element
+const myText = <Text regular /> // $ExpectType Element
+const mySpan = <Text.span regular /> // $ExpectType Element
