5e5f313478040f52ed8f47c944634fa4e5784a87
Craig Palermo <3260642+craigpalermo@users.noreply.github.com>
2020-05-29 18:47:59 +0000

    Add pcln logo to theme (#826)

---
+++ .storybook/pclnTheme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create'
+
+export default create({
+  base: 'light',
+
+  brandTitle: 'Priceline Design System',
+  brandUrl: 'https://www.priceline.com',
+  brandImage:
+    'https://press.priceline.com/wp-content/uploads/2019/10/Priceline_Logo_RGB_Blue_2019-1.png',
+})
