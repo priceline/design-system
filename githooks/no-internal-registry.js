83ad9bde888ad7b0b9b636e4d3b9ea577c4b4f37
Craig Palermo <3260642+craigpalermo@users.noreply.github.com>
2020-06-30 18:03:36 +0000

    Add pre-commit hook to screen for internal URLs in lockfiles (#858)

---
+++ githooks/no-internal-registry.js
@@ -0,0 +1,46 @@
+const { execSync } = require('child_process')
+const { readFileSync } = require('fs')
+
+function hasInternalRegistry(filePath) {
+  const file = readFileSync(filePath)
+  return file.toString().includes(/https?:\/\/npm\.prod\.pcln\.com/)
+}
+
+function getPackageLocks() {
+  const lockfiles = execSync(
+    `find . -name 'package-lock.json' -not -path "*/node_modules/*"`
+  )
+
+  return lockfiles
+    .toString()
+    .trim()
+    .split('\n')
+    .filter((s) => s !== '')
+}
+
+/**
+ * Throws an error if any PCLN internal NPM registry URLs are found in any lockfiles.
+ * @public
+ */
+function run() {
+  const lockfiles = getPackageLocks()
+
+  const foundNoInternalRegistries = lockfiles.reduce((acc, lock) => {
+    const hasInternal = hasInternalRegistry(lock)
+
+    if (hasInternal) {
+      console.warn(`Internal NPM registry found in ${lock}`)
+      return false
+    }
+
+    return acc
+  }, true)
+
+  if (!foundNoInternalRegistries) {
+    throw new Error(
+      'Internal NPM registry URLs found in lockfile(s). Please recreate them with only public NPM registries before publishing.'
+    )
+  }
+}
+
+run()
