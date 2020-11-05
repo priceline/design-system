252fbdfcc1c0f6cf07b9057be57e0001432c808d
pcln-james <james.stewart@priceline.com>
2020-11-04 18:01:51 -0600

    Add Input Variation

--- packages/core/src/Chip/Chip.js
+++ packages/core/src/Chip/Chip.js
@@ -6,8 +6,10 @@ import { applyVariations, getPaletteColor } from '../utils'
 import { Close as CloseIcon } from 'pcln-icons'
 
 const variations = {
-    choice: css`
-        ${props => props.selected && `
+  choice: css`
+    ${(props) =>
+      props.selected &&
+      `
             border: 1px solid ${getPaletteColor('base')(props)};
             background-color: ${getPaletteColor('base')(props)};
             color: ${getPaletteColor('text.lightest')(props)};
@@ -17,80 +19,97 @@ const variations = {
                 background-color: ${getPaletteColor('dark')(props)};
             }
         `}
-    `,
-    filter: css`
-        ${props => props.selected && `
+  `,
+  filter: css`
+    ${(props) =>
+      props.selected &&
+      `
             border: 1px solid ${getPaletteColor('base')(props)};
             background-color: ${getPaletteColor('light')(props)};
         `}
-    `,
-    input: css``,
+  `,
+  input: css`
+    border: 1px solid ${getPaletteColor('border.base')};
+    background-color: ${getPaletteColor('background.light')};
+    color: ${getPaletteColor('text.light')};
+
+    &:hover:not(:disabled) {
+      border: 1px solid ${getPaletteColor('border.dark')};
+      background-color: ${getPaletteColor('background.base')};
+      color: ${getPaletteColor('text.base')};
+    }
+  `,
 }
 
 const StyledChip = styled.button`
-    display: inline-flex;
-    align-items: center;
-    font-size: ${themeGet('fontSizes.0')}px;
-    color: ${getPaletteColor('base')};
-    border: 1px solid ${getPaletteColor('border.base')};
-    background-color: ${getPaletteColor('background.lightest')};
-    border-radius: 2px;
-    cursor: pointer;
-    font-family: Montserrat;
+  display: inline-flex;
+  align-items: center;
+  font-size: ${themeGet('fontSizes.0')}px;
+  color: ${getPaletteColor('base')};
+  border: 1px solid ${getPaletteColor('border.base')};
+  background-color: ${getPaletteColor('background.lightest')};
+  border-radius: 2px;
+  cursor: pointer;
+  font-family: Montserrat;
 
-    &:hover {
-        border: 1px solid ${getPaletteColor('base')};
-        background-color: ${getPaletteColor('background.light')};
-    }
+  &:hover {
+    border: 1px solid ${getPaletteColor('base')};
+    background-color: ${getPaletteColor('background.light')};
+  }
 
-    &:disabled {
-        border: 1px solid transparent;
-        background-color: ${getPaletteColor('background.light')};
-        color: ${getPaletteColor('text.light')};
-        cursor: not-allowed;
-    }
+  &:disabled {
+    border: 1px solid transparent;
+    background-color: ${getPaletteColor('background.light')};
+    color: ${getPaletteColor('text.light')};
+    cursor: not-allowed;
+  }
 
-    & > svg {
-        width: 20px;
-        height: 20px;
+  & > svg {
+    width: 20px;
+    height: 20px;
 
-        ${props => props.selected && props.variation !== 'choice' && `
+    ${(props) =>
+      props.selected &&
+      (props.variation === 'filter' || props.variation === 'input') &&
+      `
             &:last-child {
                 width: 16px;
                 height: 16px;
             }
         `}
-    }
+  }
 
-    ${space};
-    ${fontSize};
-    ${applyVariations('Chip', variations)}
+  ${space};
+  ${fontSize};
+  ${applyVariations('Chip', variations)}
 `
 
-function Chip ({ selected, variation, children, ...props }) {
-    return (
-        <StyledChip selected={selected} variation={variation} {...props}>
-            {children}
-            {selected && variation !== 'choice' && <CloseIcon ml={2} size={16} />}
-        </StyledChip>
-    )
+function Chip({ selected, variation, children, ...props }) {
+  return (
+    <StyledChip selected={selected} variation={variation} {...props}>
+      {children}
+      {selected && (variation === 'filter' || variation === 'input') && (
+        <CloseIcon ml={2} size={16} />
+      )}
+    </StyledChip>
+  )
 }
 
 Chip.displayName = 'Chip'
 
 Chip.propTypes = {
-    ...space.propTypes,
-    ...fontSize.propTypes,
-    disabled: PropTypes.bool,
-    selected: PropTypes.bool,
-    variation: PropTypes.oneOf(Object.keys(variations)),
+  ...space.propTypes,
+  ...fontSize.propTypes,
+  disabled: PropTypes.bool,
+  selected: PropTypes.bool,
+  variation: PropTypes.oneOf(Object.keys(variations)),
 }
 
 Chip.defaultProps = {
-    variation: 'filter',
-    color: 'primary',
-    px: 2,
-    py: 1,
+  variation: 'filter',
+  color: 'primary',
+  px: 2,
+  py: 1,
 }
 
 export default Chip
--- packages/core/src/Chip/Chip.stories.js
+++ packages/core/src/Chip/Chip.stories.js
@@ -2,125 +2,189 @@ import React, { useState } from 'react'
 import Chip from './Chip'
 import { Breakfast, Parking, Whirlpool, Wifi } from 'pcln-icons'
 
-function useToggle (initialValue = false) {
-    const [ value, setValue ] = useState(initialValue)
+function useToggle(initialValue = false) {
+  const [value, setValue] = useState(initialValue)
 
-    const toggle = () => setValue(!value)
+  const toggle = () => setValue(!value)
 
-    return [ value, toggle ]
+  return [value, toggle]
 }
 
-export const Choice = () => <Chip variation='choice'><Wifi mr={2} /> Free Wifi</Chip>
+export const Choice = () => (
+  <Chip variation='choice'>
+    <Wifi mr={2} /> Free Wifi
+  </Chip>
+)
 
-export const ChoiceSelected = () => <Chip selected variation='choice'><Wifi mr={2} /> Free Wifi</Chip>
+export const ChoiceSelected = () => (
+  <Chip selected variation='choice'>
+    <Wifi mr={2} /> Free Wifi
+  </Chip>
+)
 
 export const ChoiceWithState = () => {
-    const [breakfastSelected, setBreakfastSelected] = useState(true)
-    const [parkingSelected, setParkingSelected] = useState(false)
-    const [wifiSelected, setWifiSelected] = useState(false)
+  const [breakfastSelected, setBreakfastSelected] = useState(true)
+  const [parkingSelected, setParkingSelected] = useState(false)
+  const [wifiSelected, setWifiSelected] = useState(false)
 
-    const onBreakfastClick = () => {
-        setBreakfastSelected(true)
-        setParkingSelected(false)
-        setWifiSelected(false)
-    }
+  const onBreakfastClick = () => {
+    setBreakfastSelected(true)
+    setParkingSelected(false)
+    setWifiSelected(false)
+  }
 
-    const onParkingSelected = () => {
-        setBreakfastSelected(false)
-        setParkingSelected(true)
-        setWifiSelected(false)
-    }
+  const onParkingSelected = () => {
+    setBreakfastSelected(false)
+    setParkingSelected(true)
+    setWifiSelected(false)
+  }
 
-    const onWifiSelected = () => {
-        setBreakfastSelected(false)
-        setParkingSelected(false)
-        setWifiSelected(true)
-    }
+  const onWifiSelected = () => {
+    setBreakfastSelected(false)
+    setParkingSelected(false)
+    setWifiSelected(true)
+  }
 
-    return (
-        <>
-            <Chip
-                selected={breakfastSelected}
-                variation='choice'
-                onClick={onBreakfastClick}
-                m={2}
-            >
-                <Breakfast mr={2} />
-                Free Breakfast
-            </Chip>
-            <Chip
-                selected={parkingSelected}
-                variation='choice'
-                onClick={onParkingSelected}
-                m={2}
-            >
-                <Parking mr={2} />
-                Free Parking
-            </Chip>
-            <Chip
-                selected={wifiSelected}
-                variation='choice'
-                onClick={onWifiSelected}
-                m={2}
-            >
-                <Wifi mr={2} />
-                Free Wifi
-            </Chip>
-            <Chip variation='choice' disabled m={2}>
-                <Whirlpool mr={2} /> Free Whirlpool
-            </Chip>
-        </>
-    )
+  return (
+    <>
+      <Chip
+        selected={breakfastSelected}
+        variation='choice'
+        onClick={onBreakfastClick}
+        m={2}
+      >
+        <Breakfast mr={2} />
+        Free Breakfast
+      </Chip>
+      <Chip
+        selected={parkingSelected}
+        variation='choice'
+        onClick={onParkingSelected}
+        m={2}
+      >
+        <Parking mr={2} />
+        Free Parking
+      </Chip>
+      <Chip
+        selected={wifiSelected}
+        variation='choice'
+        onClick={onWifiSelected}
+        m={2}
+      >
+        <Wifi mr={2} />
+        Free Wifi
+      </Chip>
+      <Chip variation='choice' disabled m={2}>
+        <Whirlpool mr={2} /> Free Whirlpool
+      </Chip>
+    </>
+  )
 }
 
-export const Filter = () => <Chip variation='filter'><Wifi mr={2} /> Free Wifi</Chip>
+export const Filter = () => (
+  <Chip variation='filter'>
+    <Wifi mr={2} /> Free Wifi
+  </Chip>
+)
 
-export const FilterSelected = () => <Chip selected variation='filter'><Wifi mr={2} /> Free Wifi</Chip>
+export const FilterSelected = () => (
+  <Chip selected variation='filter'>
+    <Wifi mr={2} /> Free Wifi
+  </Chip>
+)
 
 export const FilterWithState = () => {
-    const [breakfastSelected, toggleBreakfastSelected] = useToggle(true)
-    const [parkingSelected, toggleParkingSelected] = useToggle(false)
-    const [wifiSelected, toggleWifiSelected] = useToggle(false)
+  const [breakfastSelected, toggleBreakfastSelected] = useToggle(true)
+  const [parkingSelected, toggleParkingSelected] = useToggle(false)
+  const [wifiSelected, toggleWifiSelected] = useToggle(false)
 
-    return (
-        <>
-            <Chip
-                selected={breakfastSelected}
-                variation='filter'
-                onClick={toggleBreakfastSelected}
-                m={2}
-            >
-                <Breakfast mr={2} />
-                Free Breakfast
-            </Chip>
-            <Chip
-                selected={parkingSelected}
-                variation='filter'
-                onClick={toggleParkingSelected}
-                m={2}
-            >
-                <Parking mr={2} />
-                Free Parking
-            </Chip>
-            <Chip
-                selected={wifiSelected}
-                variation='filter'
-                onClick={toggleWifiSelected}
-                m={2}
-            >
-                <Wifi mr={2} />
-                Free Wifi
-            </Chip>
-            <Chip variation='filter' disabled m={2}>
-                <Whirlpool mr={2} /> Free Whirlpool
-            </Chip>
-        </>
-    )
+  return (
+    <>
+      <Chip
+        selected={breakfastSelected}
+        variation='filter'
+        onClick={toggleBreakfastSelected}
+        m={2}
+      >
+        <Breakfast mr={2} />
+        Free Breakfast
+      </Chip>
+      <Chip
+        selected={parkingSelected}
+        variation='filter'
+        onClick={toggleParkingSelected}
+        m={2}
+      >
+        <Parking mr={2} />
+        Free Parking
+      </Chip>
+      <Chip
+        selected={wifiSelected}
+        variation='filter'
+        onClick={toggleWifiSelected}
+        m={2}
+      >
+        <Wifi mr={2} />
+        Free Wifi
+      </Chip>
+      <Chip variation='filter' disabled m={2}>
+        <Whirlpool mr={2} /> Free Whirlpool
+      </Chip>
+    </>
+  )
 }
 
-export const Disabled = () => <Chip disabled><Wifi mr={2} /> Free Wifi</Chip>
+export const Input = () => (
+  <Chip variation='input'>
+    <Wifi mr={2} /> Free Wifi
+  </Chip>
+)
+
+export const InputSelected = () => (
+  <Chip variation='input' selected>
+    <Wifi mr={2} /> Free Wifi
+  </Chip>
+)
+
+export const InputWithState = () => {
+  const [breakfastEnabled, toggleBreakfast] = useToggle(true)
+  const [parkingEnabled, toggleParking] = useToggle(true)
+  const [wifiEnabled, toggleWifi] = useToggle(true)
+
+  return (
+    <>
+      {breakfastEnabled && (
+        <Chip selected variation='input' onClick={toggleBreakfast} m={2}>
+          <Breakfast mr={2} />
+          Free Breakfast
+        </Chip>
+      )}
+      {parkingEnabled && (
+        <Chip selected variation='input' onClick={toggleParking} m={2}>
+          <Parking mr={2} />
+          Free Parking
+        </Chip>
+      )}
+      {wifiEnabled && (
+        <Chip selected variation='input' onClick={toggleWifi} m={2}>
+          <Wifi mr={2} />
+          Free Wifi
+        </Chip>
+      )}
+      <Chip variation='input' disabled m={2}>
+        <Whirlpool mr={2} /> Free Whirlpool
+      </Chip>
+    </>
+  )
+}
+
+export const Disabled = () => (
+  <Chip disabled>
+    <Wifi mr={2} /> Free Wifi
+  </Chip>
+)
 
 export default {
-    title: 'Chip',
-    component: Chip
+  title: 'Chip',
+  component: Chip,
 }
