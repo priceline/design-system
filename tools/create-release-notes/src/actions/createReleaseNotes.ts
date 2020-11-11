cfde81ae7ff02fbd9bbf43acce370e0325fe7425
Craig Palermo <3260642+craigpalermo@users.noreply.github.com>
2020-11-11 09:19:39 -0500

    Automate release notes (#950)
    * Port over tool to automatically create release notes from changelog
    
    * Apply changes for core 3.6.2
    
    * Create action to automate release creation
    
    * Manually trigger change in core

---
+++ tools/create-release-notes/src/actions/createReleaseNotes.ts
@@ -0,0 +1,102 @@
+import {
+  RushConfiguration,
+  RushConfigurationProject,
+} from '@microsoft/rush-lib'
+import fs from 'fs'
+import { join } from 'path'
+import { Release, Change } from '../types/Release'
+
+const config = RushConfiguration.loadFromDefaultLocation()
+
+const COMMIT_URL_STEM = 'https://github.com/priceline/design-system/commit/'
+
+const _getChangelog = (project: RushConfigurationProject) => {
+  const file = fs.readFileSync(join(project.projectFolder, 'CHANGELOG.json'))
+  return JSON.parse(file.toString())
+}
+
+const _getCommitUrl = (commitHash: string): string => {
+  return `${COMMIT_URL_STEM}${commitHash}`
+}
+
+const _parseAuthor = (author: string): { name; email? } => {
+  const re = /([\w\s]+)(<?(.*)>)?$/
+  const matches = re.exec(author)
+  return {
+    name: matches[1],
+    email: matches[2],
+  }
+}
+
+const _parseComment = (comment: string) => {
+  const re = /^.*(\w{3,4}-\d+).*$/
+  const matches = re.exec(comment)
+  return {
+    jiraId: matches ? matches[1] : '',
+  }
+}
+
+const _formatChangeRow = (change: Change): string => {
+  const author = _parseAuthor(change.author)
+  const authorName = author.name !== 'undefined' ? author.name : 'Rush'
+  let commitLink = ''
+
+  if (change.commit) {
+    const commitHash = change.commit.slice(0, 8)
+
+    // TODO figure out how to go to commit page without branch name
+    commitLink = commitHash
+  } else {
+    commitLink = `n/a`
+  }
+
+  return `|${change.type}|${change.comment}|${authorName.trim()}|${commitLink}|`
+}
+
+export const _transformRushComments = (comments): Change[] => {
+  return ['major', 'minor', 'patch', 'dependency'].reduce((acc, cur) => {
+    if (!comments[cur]) return acc
+
+    const newComments = comments[cur].map((comment) => ({
+      ...comment,
+      type: cur,
+    }))
+
+    return [...acc, ...newComments]
+  }, [])
+}
+
+export const formatNotesMarkdown = (notes: Release): string => {
+  return `# \`${notes.packageName}\` v${notes.version}
+
+> Created ${notes.date}
+
+|Type | Change | Author | Commit |
+|-----|--------|--------|--------|
+${notes.changes.map(_formatChangeRow).join('\n')}
+`
+}
+
+export const createReleaseNotes = (packageName): Release => {
+  const project = config.getProjectByName(packageName)
+
+  if (!project) {
+    throw new Error('Project not found')
+  }
+
+  const changeLog = _getChangelog(project)
+
+  if (!project) {
+    throw new Error('Changelog not found')
+  }
+
+  const latestChange = changeLog.entries[0]
+
+  return {
+    packageName: packageName,
+    changes: _transformRushComments(latestChange.comments),
+    date: latestChange.date,
+    tag: latestChange.tag,
+    version: latestChange.version,
+  }
+}
---
+++ tools/create-release-notes/src/cli/releaseNotes.ts
@@ -0,0 +1,29 @@
+import { argv } from 'yargs'
+import fs from 'fs'
+import { join } from 'path'
+import { RushConfiguration } from '@microsoft/rush-lib'
+import {
+  createReleaseNotes,
+  formatNotesMarkdown,
+} from '../actions/createReleaseNotes'
+
+const { packageName } = argv
+
+const config = RushConfiguration.loadFromDefaultLocation()
+
+console.log('Creating release notes...')
+
+if (packageName) {
+  const notes = createReleaseNotes(packageName)
+
+  console.log('Creating release notes... SUCCESS')
+
+  console.log(formatNotesMarkdown(notes))
+
+  fs.writeFileSync(
+    join(config.rushJsonFolder, 'RELEASE_NOTES.md'),
+    formatNotesMarkdown(notes)
+  )
+
+  console.log('Wrote release notes to file')
+}
