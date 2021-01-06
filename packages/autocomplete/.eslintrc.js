a48cb6ef05230f13113538899d7319cc8f9e8697
Craig Palermo <3260642+craigpalermo@users.noreply.github.com>
2021-01-06 12:37:17 -0500

    Fix cyclic dependency that will cause pcln-icons tests to fail in future (#995)
    Co-authored-by: Craig Palermo <email@craigp.me>

--- packages/autocomplete/.eslintrc.js
+++ packages/autocomplete/.eslintrc.js
@@ -1,3 +1,4 @@
+require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   extends: ['@priceline', '@priceline/eslint-config/jsx-a11y'],
   parserOptions: { tsconfigRootDir: __dirname },
