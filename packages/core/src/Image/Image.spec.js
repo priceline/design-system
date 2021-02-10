6966790a810edc44c0d71242fe5a888f703273f2
James <62619213+pcln-james@users.noreply.github.com>
2021-02-10 12:43:15 -0600

    Image height and spacing (#1014)
    * Image height and spacing
    
    * Changelog

--- packages/core/src/Image/Image.spec.js
+++ packages/core/src/Image/Image.spec.js
@@ -1,13 +1,44 @@
 import React from 'react'
-
+import { render, screen } from 'testing-library'
 import { Image } from '..'
 
 const imageSrc =
-  'https://www.qaa.priceline.com/home/public/735e9d77a0adaeaf0c46845da8c5db8b.jpg'
+  'https://www.goodfreephotos.com/albums/new-zealand/other-new-zealand/lake-pukaki-and-mount-cook-in-the-background.jpg'
 
 describe('Image', () => {
-  test('renders', () => {
-    const json = rendererCreateWithTheme(<Image src={imageSrc} />).toJSON()
-    expect(json).toMatchSnapshot()
+  it('renders', () => {
+    render(<Image src={imageSrc} alt='new-zealand-image' />)
+    const image = screen.getByAltText('new-zealand-image')
+    expect(image).toHaveStyleRule('max-width', '100%')
+    expect(image).not.toHaveAttribute('width')
+    expect(image).not.toHaveAttribute('height')
+  })
+
+  it('renders with fixed width', () => {
+    render(<Image src={imageSrc} alt='new-zealand-image' width='500px' />)
+    const image = screen.getByAltText('new-zealand-image')
+    expect(image).toHaveAttribute('width', '500px')
+    expect(image).not.toHaveAttribute('height')
+  })
+
+  it('renders with fixed width and height', () => {
+    render(
+      <Image
+        src={imageSrc}
+        alt='new-zealand-image'
+        width='500px'
+        height='500px'
+      />
+    )
+    const image = screen.getByAltText('new-zealand-image')
+    expect(image).toHaveAttribute('width', '500px')
+    expect(image).toHaveAttribute('height', '500px')
+  })
+
+  it('renders with spacing', () => {
+    render(<Image src={imageSrc} alt='new-zealand-image' pt={2} mt={3} />)
+    const image = screen.getByAltText('new-zealand-image')
+    expect(image).toHaveStyleRule('padding-top', '8px')
+    expect(image).toHaveStyleRule('margin-top', '16px')
   })
 })
--- packages/core/src/Image/Image.stories.js
+++ packages/core/src/Image/Image.stories.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Image } from '..'
 
 const description = 'A low-level layout component that renders an image'
+const imageSrc =
+  'https://www.goodfreephotos.com/albums/new-zealand/other-new-zealand/lake-pukaki-and-mount-cook-in-the-background.jpg'
 
 export default {
   title: 'Image',
@@ -15,13 +17,10 @@ export default {
   },
 }
 
-export const Default = () => (
-  <Image src='https://www.priceline.com/home/public/assets/images/photos/photo-aruba.jpg' />
-)
+export const Default = () => <Image src={imageSrc} />
 
-export const ResponsiveWidth = () => (
-  <Image
-    width={1 / 2}
-    src='https://www.priceline.com/home/public/assets/images/photos/photo-aruba.jpg'
-  />
+export const ResponsiveWidth = () => <Image width={1 / 2} src={imageSrc} />
+
+export const SpecificWidthAndHeight = () => (
+  <Image width='200px' height='300px' src={imageSrc} />
 )
