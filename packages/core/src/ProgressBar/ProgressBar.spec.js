761d993bd87c727d6528de38fd9ccaaf53730ff7
JonChoi16 <77304994+JonChoi16@users.noreply.github.com>
2021-02-22 15:23:29 -0600

    UXPT-1163: progress bar (#1022)
    * add progress bar component
    
    * add a prop for different bar colors
    
    * add additional props
    
    * Small Rework
    
    * add a testing suite for ProgressBar component
    
    * add additional tests for ProgressBar
    
    * deleted renderer from test file
    
    * changed a prop name and the value of key prop
    
    * add a className prop
    
    * rush change
    
    * add change log
    
    * .
    
    * delete change log
    
    Co-authored-by: pcln-james <james.stewart@priceline.com>

---
+++ packages/core/src/ProgressBar/ProgressBar.spec.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { render, screen } from 'testing-library'
+import ProgressBar from './ProgressBar'
+
+const testProps = [
+  { color: 'warning' },
+  { color: 'caution' },
+  { color: 'primary' },
+  { color: 'success' },
+]
+
+describe('ProgressBar', () => {
+  it('Default progress bar (no colors)', () => {
+    render(<ProgressBar steps={testProps} currentStep={0} />)
+    const firstBar = screen.getByTestId('test-id-0')
+    const secondBar = screen.getByTestId('test-id-1')
+    const thirdBar = screen.getByTestId('test-id-2')
+    const fourthBar = screen.getByTestId('test-id-3')
+    expect(firstBar).toHaveStyleRule('background-color', '#f4f6f8')
+    expect(secondBar).toHaveStyleRule('background-color', '#f4f6f8')
+    expect(thirdBar).toHaveStyleRule('background-color', '#f4f6f8')
+    expect(fourthBar).toHaveStyleRule('background-color', '#f4f6f8')
+  })
+
+  it('Basic progress bar with step level 3', () => {
+    render(<ProgressBar steps={testProps} currentStep={3} />)
+    const firstBar = screen.getByTestId('test-id-0')
+    const secondBar = screen.getByTestId('test-id-1')
+    const thirdBar = screen.getByTestId('test-id-2')
+    const fourthBar = screen.getByTestId('test-id-3')
+    expect(firstBar).toHaveStyleRule('background-color', '#007aff')
+    expect(secondBar).toHaveStyleRule('background-color', '#007aff')
+    expect(thirdBar).toHaveStyleRule('background-color', '#007aff')
+    expect(fourthBar).toHaveStyleRule('background-color', '#f4f6f8')
+  })
+
+  it('Basic progress bar with step level 4', () => {
+    render(<ProgressBar steps={testProps} currentStep={4} />)
+    const firstBar = screen.getByTestId('test-id-0')
+    const secondBar = screen.getByTestId('test-id-1')
+    const thirdBar = screen.getByTestId('test-id-2')
+    const fourthBar = screen.getByTestId('test-id-3')
+    expect(firstBar).toHaveStyleRule('background-color', '#0a0')
+    expect(secondBar).toHaveStyleRule('background-color', '#0a0')
+    expect(thirdBar).toHaveStyleRule('background-color', '#0a0')
+    expect(fourthBar).toHaveStyleRule('background-color', '#0a0')
+  })
+})
---
+++ packages/core/src/ProgressBar/ProgressBar.stories.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Box } from '../Box'
+import ProgressBar from './ProgressBar'
+
+export default {
+  title: 'ProgressBar',
+  component: ProgressBar,
+}
+
+const steps = [
+  { color: 'warning' },
+  { color: 'caution' },
+  { color: 'primary' },
+  { color: 'success' },
+]
+
+export const basicProgressBar = () => (
+  <ProgressBar steps={steps} currentStep={3} />
+)
+
+export const Thick = () => (
+  <ProgressBar steps={steps} currentStep={1} stepHeight='10px' />
+)
+
+export const Short = () => (
+  <Box width='300px'>
+    <ProgressBar steps={steps} currentStep={4} />
+  </Box>
+)
