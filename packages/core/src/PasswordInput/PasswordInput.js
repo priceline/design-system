7b5dc7e895420c458b39276763915772a2c69359
Jon Choi <jon.choi@priceline.com>
2021-03-02 13:22:03 -0600

    add check icon

--- packages/core/src/PasswordInput/PasswordInput.js
+++ packages/core/src/PasswordInput/PasswordInput.js
@@ -4,12 +4,19 @@ import {
   Flex,
   Text,
   Input,
+  Label,
   IconField,
   Badge,
   ProgressBar,
   IconButton,
 } from '../../src'
 import { Visibility, VisibilityOff, Check } from 'pcln-icons'
+import styled from 'styled-components'
+import { themeGet } from 'styled-system'
+
+const CustomLabel = styled(Label)`
+  font-size: ${themeGet('fontSizes.0')}px;
+`
 
 function PasswordInput({
   label,
@@ -29,25 +36,32 @@ function PasswordInput({
 
   const [inputPassword, setInputPassword] = useState('')
   const [strengthLevel, setStrengthLevel] = useState(progressBarDefaultStep)
-
+  const [showCheckIcon, setShowCheckIcon] = useState(false)
   const handleChange = (event) => {
     const currentInput = event.target.value
+    const progressBarLength = 4
+    let strengthPoint = 0
     setInputPassword(currentInput)
 
-    let strengthPoint = 0
     regexChecks.forEach((element) => {
       element.regex.test(currentInput) && strengthPoint++
     })
-    strengthPoint == 0 ? setStrengthLevel(1) : setStrengthLevel(strengthPoint)
+    strengthPoint < regexChecks.length - 2
+      ? setStrengthLevel(1)
+      : setStrengthLevel(
+          progressBarLength - (regexChecks.length - strengthPoint)
+        )
+    strengthPoint == regexChecks.length
+      ? setShowCheckIcon(true)
+      : setShowCheckIcon(false)
   }
 
   return (
     <Flex flexDirection='column'>
-      <Text bold color='text.light' mb={2}>
-        {label}
-      </Text>
+      <CustomLabel mb={2}>{label}</CustomLabel>
       <IconField>
         <Input type={inputType} onChange={handleChange} />
+        {showCheckIcon && <Check color='secondary' />}
         <IconButton
           title='visibility button'
           icon={visibilityIcon}
@@ -88,7 +102,8 @@ PasswordInput.defaultProps = {
     { label: '1 Uppercase Letter', regex: /(?=.*[A-Z])/ },
     { label: '1 Lowercase Letter', regex: /(?=.*[a-z])/ },
     { label: '1 Number', regex: /(?=.*[0-9])/ },
-    { label: '1 Special Charater', regex: /(?=.*[!@#$%^&*()])/ },
+    { label: '1 Special Character', regex: /(?=.*[!@#$%^&*()])/ },
+    { label: 'at least 8 Characters', regex: /.{8,}/ },
   ],
 }
 
