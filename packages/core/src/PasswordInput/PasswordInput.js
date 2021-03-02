c35eee037958db7e687569d5257216dc3637e897
Jon Choi <jon.choi@priceline.com>
2021-03-02 17:14:28 -0600

    add additional checks for password

--- packages/core/src/PasswordInput/PasswordInput.js
+++ packages/core/src/PasswordInput/PasswordInput.js
@@ -1,5 +1,14 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { themeGet } from 'styled-system'
+import {
+  Visibility,
+  VisibilityOff,
+  Check,
+  Success,
+  SuccessOutline,
+} from 'pcln-icons'
 import {
   Flex,
   Text,
@@ -10,11 +19,13 @@ import {
   ProgressBar,
   IconButton,
 } from '../../src'
-import { Visibility, VisibilityOff, Check } from 'pcln-icons'
-import styled from 'styled-components'
-import { themeGet } from 'styled-system'
 
-const CustomLabel = styled(Label)`
+const CustomFlex = styled(Flex)`
+  margin-left: ${themeGet('space.3')}px;
+  align-contents: center;
+`
+
+const CustomText = styled(Text)`
   font-size: ${themeGet('fontSizes.0')}px;
 `
 
@@ -37,28 +48,51 @@ function PasswordInput({
   const [inputPassword, setInputPassword] = useState('')
   const [strengthLevel, setStrengthLevel] = useState(progressBarDefaultStep)
   const [showCheckIcon, setShowCheckIcon] = useState(false)
+  const [hasNumOrSpecial, setHasNumOrSpecial] = useState(false)
+  const [hasMinEightChar, setHasMinEightChar] = useState(false)
+
+  const successIconOn = <Success size='12px' color={'secondary'} mr={1} />
+  const successIconOff = (
+    <SuccessOutline size='12px' color={'text.light'} mr={1} />
+  )
+
+  const successIconOne = hasNumOrSpecial ? successIconOn : successIconOff
+  const successIconTwo = hasMinEightChar ? successIconOn : successIconOff
+  const requirementOneColor = hasNumOrSpecial ? 'secondary' : 'text.light'
+  const requirementTwoColor = hasMinEightChar ? 'secondary' : 'text.light'
+
   const handleChange = (event) => {
     const currentInput = event.target.value
     const progressBarLength = 4
+    const hasNumberOrSpecial =
+      regexChecks[2].regex.test(currentInput) ||
+      regexChecks[3].regex.test(currentInput)
+    const hasMinimumEight = regexChecks[4].regex.test(currentInput)
     let strengthPoint = 0
     setInputPassword(currentInput)
 
     regexChecks.forEach((element) => {
       element.regex.test(currentInput) && strengthPoint++
     })
+
     strengthPoint < regexChecks.length - 2
       ? setStrengthLevel(1)
       : setStrengthLevel(
           progressBarLength - (regexChecks.length - strengthPoint)
         )
-    strengthPoint == regexChecks.length
+
+    hasNumberOrSpecial ? setHasNumOrSpecial(true) : setHasNumOrSpecial(false)
+    hasMinimumEight ? setHasMinEightChar(true) : setHasMinEightChar(false)
+    hasNumberOrSpecial && hasMinimumEight
       ? setShowCheckIcon(true)
       : setShowCheckIcon(false)
   }
 
   return (
     <Flex flexDirection='column'>
-      <CustomLabel mb={2}>{label}</CustomLabel>
+      <Label fontSize={0} mb={2}>
+        {label}
+      </Label>
       <IconField>
         <Input type={inputType} onChange={handleChange} />
         {showCheckIcon && <Check color='secondary' />}
@@ -70,19 +104,33 @@ function PasswordInput({
       </IconField>
       {hasProgressBar && (
         <Flex flexDirection='column'>
-          <Flex alignContent='center' mb={2}>
-            <Text color='text.light' mt={2}>
+          <Flex alignItems='center' mt={2} mb={2}>
+            <CustomText color='text.light' mr={2}>
               Password Strength:{' '}
-            </Text>
+            </CustomText>
             <Badge
               color={progressBarSteps[strengthLevel - 1].color}
-              mt={2}
-              ml={2}
+              size='small'
             >
               {progressBarSteps[strengthLevel - 1].text}
             </Badge>
           </Flex>
           <ProgressBar steps={progressBarSteps} currentStep={strengthLevel} />
+          <Flex align-items='center' mt={2}>
+            <CustomText color='text.light'>Must contain:</CustomText>
+            <CustomFlex alignItems='center'>
+              {successIconOne}
+              <CustomText color={requirementOneColor}>
+                1 number or special character
+              </CustomText>
+            </CustomFlex>
+            <CustomFlex alignItems='center'>
+              {successIconTwo}
+              <CustomText color={requirementTwoColor}>
+                8 characters minimum
+              </CustomText>
+            </CustomFlex>
+          </Flex>
         </Flex>
       )}
     </Flex>
