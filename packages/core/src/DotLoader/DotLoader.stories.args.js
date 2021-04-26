fd99eaed6e45a851c980ed752bf37c4286350e7a
James <62619213+pcln-james@users.noreply.github.com>
2021-04-26 12:26:42 -0500

    Created DotLoader component (#1067)
    * Created DotLoader component
    
    * export components
    
    * Changelog
    
    * Simplify the DotLoader
    
    * Improve Tests
    
    * use themeGet

---
+++ packages/core/src/DotLoader/DotLoader.stories.args.js
@@ -0,0 +1,64 @@
+import { colors } from '../../storybook/args'
+
+export const sizes = ['small', 'medium', 'large']
+
+export const speed = ['slow', 'medium', 'fast']
+
+export const defaultArgs = {
+  size: 'medium',
+  speed: 'medium',
+  color: 'background',
+}
+
+export const argTypes = {
+  size: {
+    name: 'size',
+    type: { name: 'string', required: false },
+    description: 'Size to render.\nAccepts string or a number.',
+    table: {
+      type: {
+        summary: 'Sizes',
+        detail: '- ' + sizes.join('\n- '),
+      },
+      defaultValue: { summary: 'medium' },
+    },
+    control: {
+      type: 'select',
+      options: sizes,
+    },
+  },
+
+  speed: {
+    name: 'speed',
+    type: { name: 'string', required: false },
+    description: 'Speed to render.\nAccepts string or a number.',
+    table: {
+      type: {
+        summary: 'Speeds',
+        detail: '- ' + speed.join('\n- '),
+      },
+      defaultValue: { summary: 'medium' },
+    },
+    control: {
+      type: 'select',
+      options: speed,
+    },
+  },
+
+  color: {
+    name: 'color',
+    type: { name: 'string', required: false },
+    description: 'Palette color to use',
+    table: {
+      type: {
+        summary: 'Colors',
+        detail: '- ' + colors.join('\n- '),
+      },
+      defaultValue: { summary: 'background' },
+    },
+    control: {
+      type: 'select',
+      options: colors,
+    },
+  },
+}
---
+++ packages/core/src/DotLoader/index.js
@@ -0,0 +1,1 @@
+export { default as DotLoader } from './DotLoader'
