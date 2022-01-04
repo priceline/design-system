cb05489f90c19b7deefabf7301d7c6de0617cca4
Craig Palermo <3260642+craigpalermo@users.noreply.github.com>
2022-01-04 13:19:50 -0500

    Upgrade next.js in docs project to fix broken postinstall

--- apps/docs/.eslintrc.js
+++
@@ -1,5 +0,0 @@
-require('@rushstack/eslint-patch/modern-module-resolution')
-module.exports = {
-  extends: ['@priceline', '@priceline/eslint-config/jsx-a11y'],
-  parserOptions: { tsconfigRootDir: __dirname },
-}
