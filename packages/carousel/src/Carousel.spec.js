2903c6e1724273f4d2fe57d130a58e47e139279f
rhunt <ryan.hunt@priceline.com>
2022-01-27 11:49:53 -0500

    Carousel: update unit tests

--- packages/carousel/src/Carousel.spec.js
+++ packages/carousel/src/Carousel.spec.js
@@ -4,9 +4,23 @@ import { Flex } from 'pcln-design-system'
 import { Carousel } from './Carousel'
 
 describe('Carousel', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    })
+  })
+
   it('should render a carousel', () => {
     const { getByText } = render(
-      <Carousel>
+      <Carousel showDots arrowsPosition='side'>
         <Flex>Slide 1</Flex>
         <Flex>Slide 2</Flex>
         <Flex>Slide 3</Flex>
@@ -18,7 +32,7 @@ describe('Carousel', () => {
 
   it('should set slide widths if layout is set', () => {
     const { getByTestId } = render(
-      <Carousel layout='75-25'>
+      <Carousel layout='75-25' arrowsPosition='bottom'>
         <Flex>Slide 1</Flex>
         <Flex>Slide 2</Flex>
         <Flex>Slide 3</Flex>
