1a8447a0fd8e5fc4557c8a1cd45d49b5a7bb3390
Craig Palermo <craig.palermo@priceline.com>
2022-05-24 15:52:35 -0400

    Move Button docs to mdx

--- tools/storybook-preset/src/index.ts
+++ tools/storybook-preset/src/index.ts
@@ -3,10 +3,10 @@ export * from '@storybook/addon-viewport'
 export * from 'storybook-addon-performance'
 
 /** @public */
-export const heftStoryGlobs = ['../lib-esm/**/*.stories.mdx', '../lib-esm/**/*.stories.@(js|jsx|ts|tsx)']
+export const heftStoryGlobs = ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)']
 
 /** @public */
-export const babelStoryGlobs = ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)']
+export const babelStoryGlobs = ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)']
 
 /** @public */
 export const storykitAddons = [
