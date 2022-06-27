972009ad3279282fc53353c6ae77f540c428ccb5
Ralston Almeida <ralston.almeida@priceline.com>
2022-06-27 13:31:27 -0400

    Add variations for round icon button

--- packages/core/src/IconButton/IconButton.stories.tsx
+++ packages/core/src/IconButton/IconButton.stories.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
-import { Calendar } from 'pcln-icons'
-
-import { Button, IconButton } from '..'
+import { Calendar, Image } from 'pcln-icons'
+import { Box, Button, Flex, IconButton, Text } from '..'
 import ForwardRefDemo from '../storybook/utils/ForwardRefsDemo'
 
 export default {
@@ -11,43 +10,236 @@ export default {
 }
 
 export const Default = () => (
-  <IconButton
-    onClick={action('Clicked IconButton')}
-    aria-label={'Click to choose date'}
-    icon={<Calendar title='Choose date' />}
-  />
+  <Box>
+    <h3>Default</h3>
+    <IconButton
+      onClick={action('Clicked IconButton')}
+      aria-label={'Click to choose date'}
+      icon={<Image title='Choose date' />}
+    />
+    <Box mt={4} />
+    <h3> Color</h3>
+    <Flex>
+      <Box>
+        <Text>Primary</Text>
+        <IconButton
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' color='primary' />}
+          variation='plain'
+        />
+      </Box>
+      <Box ml={4}>
+        <Text>Secondary</Text>
+        <IconButton
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' color='secondary' />}
+          variation='plain'
+        />
+      </Box>
+    </Flex>
+    <Box mt={4} />
+    <h3> Size</h3>
+    <Flex>
+      <Box>
+        <Text>24px (default)</Text>
+        <IconButton
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' color='primary' />}
+          variation='plain'
+        />
+      </Box>
+      <Box ml={4}>
+        <Text>32px</Text>
+        <IconButton
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' color='primary' size={32} />}
+          variation='plain'
+        />
+      </Box>
+      <Box ml={4}>
+        <Text>32px</Text>
+        <IconButton
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' color='primary' size={48} />}
+          variation='plain'
+        />
+      </Box>
+      <Box ml={4}>
+        <Text>32px</Text>
+        <IconButton
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' color='primary' size={64} />}
+          variation='plain'
+        />
+      </Box>
+    </Flex>
+    <Box mt={4} />
+    <h3> Disabled</h3>
+    <IconButton onClick={action('Clicked IconButton')} icon={<Calendar title='Choose date' />} disabled />
+    <Box mt={4} />
+    <h3> containerSize</h3>
+    <Flex>
+      <Box>
+        <Text pr={4} pb={3}>
+          sm
+        </Text>
+        <IconButton
+          containerSize='sm'
+          variation='fill'
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+        />
+      </Box>
+      <Box px={4}>
+        <Text pb={3}>md (default)</Text>
+        <IconButton
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+          containerSize='md'
+          variation='fill'
+        />
+      </Box>
+      <Box px={4}>
+        <Text pb={3}>lg</Text>
+        <IconButton
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+          containerSize='lg'
+          variation='fill'
+        />
+      </Box>
+      <Box px={4}>
+        <Text pb={3}>xl</Text>
+        <IconButton
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+          containerSize='xl'
+          variation='fill'
+        />
+      </Box>
+    </Flex>
+    <Box mt={4} />
+    <h3> Variation</h3>
+    <Flex>
+      <Box>
+        <Text pb={3}>Fill (default)</Text>
+        <IconButton
+          variation='fill'
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+        />
+      </Box>
+      <Box px={4}>
+        <Text pb={3}>Subtle</Text>
+        <IconButton
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+          variation='subtle'
+        />
+      </Box>
+      <Box px={4}>
+        <Text pb={3}>lightFill</Text>
+        <IconButton
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+          variation='lightFill'
+        />
+      </Box>
+      <Box px={4}>
+        <Text pb={3}>outline</Text>
+        <IconButton
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+          variation='outline'
+        />
+      </Box>
+      <Box bg='caution.light' px={4}>
+        <Text pb={3}>plain</Text>
+        <IconButton
+          p={3}
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+          variation='plain'
+        />
+      </Box>
+      <Box bg='caution.light' px={4} pb={4}>
+        <Text pb={3}>white</Text>
+        <IconButton
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+          variation='white'
+        />
+      </Box>
+    </Flex>
+    <Box mt={4} />
+    <h3> boxShadowSize</h3>
+    <Flex>
+      <Box pb={4}>
+        <Text pb={3}>sm</Text>
+        <IconButton
+          boxShadowSize='sm'
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+          variation='white'
+        />
+      </Box>
+      <Box px={4} pb={4}>
+        <Text pb={3}>md</Text>
+        <IconButton
+          boxShadowSize='md'
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+          variation='white'
+        />
+      </Box>
+      <Box pr={4} pb={4}>
+        <Text pb={3}>lg</Text>
+        <IconButton
+          boxShadowSize='lg'
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+          variation='white'
+        />
+      </Box>
+      <Box pr={4} pb={4}>
+        <Text pb={3}>xl</Text>
+        <IconButton
+          boxShadowSize='xl'
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+          variation='white'
+        />
+      </Box>
+      <Box pr={4} pb={4}>
+        <Text pb={3}>2xl</Text>
+        <IconButton
+          boxShadowSize='2xl'
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+          variation='white'
+        />
+      </Box>
+      <Box pr={4} pb={4}>
+        <Text pb={3}>overlay-lg</Text>
+        <IconButton
+          boxShadowSize='overlay-lg'
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+          variation='white'
+        />
+      </Box>
+      <Box pr={4} pb={4}>
+        <Text pb={3}>overlay-xl</Text>
+        <IconButton
+          boxShadowSize='overlay-xl'
+          onClick={action('Clicked IconButton')}
+          icon={<Calendar title='Choose date' />}
+          variation='white'
+        />
+      </Box>
+    </Flex>
+  </Box>
 )
-
-Default.story = {
-  name: 'default',
-}
-
-export const WithColor = () => (
-  <IconButton
-    onClick={action('Clicked IconButton')}
-    icon={<Calendar title='Choose date' color='primary' />}
-  />
-)
-
-WithColor.story = {
-  name: 'with color',
-}
-
-export const WithSize = () => (
-  <IconButton onClick={action('Clicked IconButton')} icon={<Calendar title='Choose date' size={64} />} />
-)
-
-WithSize.story = {
-  name: 'with size',
-}
-
-export const WithDisabled = () => (
-  <IconButton onClick={action('Clicked IconButton')} icon={<Calendar title='Choose date' />} disabled />
-)
-
-WithDisabled.story = {
-  name: 'with disabled',
-}
 
 export function ForwardRefs() {
   function refChild(dsRef) {
--- packages/core/src/IconButton/IconButton.tsx
+++ packages/core/src/IconButton/IconButton.tsx
@@ -1,9 +1,104 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import PropTypes, { InferProps } from 'prop-types'
 
 import { Button } from '../Button'
-import { applyVariations } from '../utils'
+import { applyVariations, getPaletteColor, getTextColorOn } from '../utils'
+
+const variations = {
+  fill: css`
+    background-color: ${(props) => getPaletteColor(props.disabled ? 'light' : 'base')(props)};
+    color: ${(props) => getTextColorOn(props.disabled ? 'light' : 'base')(props)};
+
+    &:hover {
+      background-color: ${(props) => getPaletteColor(props.disabled ? 'light' : 'dark')(props)};
+      ${(props) => (props.disabled ? '' : `color: ${getTextColorOn('dark')(props)};`)}
+    }
+    &:focus {
+      outline: ${(props) => `0px solid ${getPaletteColor(props.disabled ? '' : 'dark')(props)}`};
+      box-shadow: ${(props) => ` 0 0 0 2px  ${getPaletteColor(props.disabled ? '' : 'dark')(props)}`};
+    }
+  `,
+  outline: css`
+    color: ${(props) => getPaletteColor(props.disabled ? 'light' : 'base')(props)};
+    box-shadow: inset 0 0 0 2px ${(props) => getPaletteColor(props.disabled ? 'light' : 'base')(props)};
+    background-color: transparent;
+
+    &:hover {
+      background-color: transparent;
+      ${(props) =>
+        props.disabled
+          ? ''
+          : `
+      color: ${getPaletteColor('dark')(props)};
+      box-shadow: inset 0 0 0 2px ${getPaletteColor('dark')(props)};
+    `}
+    }
+  `,
+  plain: css`
+    background-color: transparent;
+    color: ${getPaletteColor('base')};
+    &:hover {
+      background-color: ${getPaletteColor('background.light')};
+      color: ${getPaletteColor('dark')};
+    }
+    &:focus {
+      outline: 0px solid ${getPaletteColor('dark')};
+      box-shadow: 0 0 0 2px ${getPaletteColor('dark')};
+    }
+  `,
+  subtle: css`
+    background-color: ${getPaletteColor('background.base')};
+    color: ${getPaletteColor('base')};
+    &:hover {
+      background-color: ${getPaletteColor('background.light')};
+      color: ${getPaletteColor('dark')};
+    }
+    &:focus {
+      outline: 0px solid ${getPaletteColor('dark')};
+      box-shadow: 0 0 0 2px ${getPaletteColor('dark')};
+    }
+  `,
+  white: css`
+    background-color: ${getPaletteColor('background.lightest')};
+    color: ${getPaletteColor('base')};
+    &:hover {
+      background-color: ${getPaletteColor('background.light')};
+      color: ${getPaletteColor('dark')};
+    }
+    &:focus {
+      outline: 0px solid ${getPaletteColor('dark')};
+      box-shadow: 0 0 0 2px ${getPaletteColor('dark')};
+    }
+  `,
+  lightFill: css`
+    background-color: ${getPaletteColor('primary.light')};
+    color: ${getPaletteColor('base')};
+    &:hover {
+      background-color: ${getPaletteColor('light')};
+      color: ${getPaletteColor('dark')};
+    }
+    &:focus {
+      outline: ${(props) => `0px solid ${getPaletteColor(props.disabled ? '' : 'dark')(props)}`};
+      box-shadow: ${(props) => ` 0 0 0 2px  ${getPaletteColor(props.disabled ? '' : 'dark')(props)}`};
+    }
+  `,
+}
+
+const getPadding = (containerSize) => {
+  switch (containerSize) {
+    case 'small':
+      return '8px'
+    case 'medium':
+      return '10px'
+    case 'large':
+      return '12px'
+    case 'xlarge':
+      return '16px'
+    default:
+      return '10px'
+  }
+}
 
 const TransparentButton = styled(Button)`
   padding: 0;
@@ -20,8 +115,9 @@ const TransparentButton = styled(Button)`
   &:disabled {
     background-color: transparent;
   }
+  ${applyVariations('IconButton', variations)}
 
-  ${applyVariations('IconButton')}
+  padding: ${(props) => getPadding(props.containerSize)};
 `
 
 const propTypes = {
@@ -38,6 +134,11 @@ const IconButton: React.FC<InferProps<typeof propTypes>> = React.forwardRef(({ i
 
 IconButton.displayName = 'IconButton'
 IconButton.isIconButton = true
+
+IconButton.defaultProps = {
+  variation: 'plain',
+  containerSize: 'medium',
+}
 
 IconButton.propTypes = propTypes
 
