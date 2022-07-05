c22827629833b0762d0586ad4775c8535eaa9149
Byro Mitchell <byron.mitchell@priceline.com>
2022-07-05 13:12:12 -0400

    feat(indeterminate-checkbox): add support for indeterminate checkboxes

--- packages/core/src/Checkbox/Checkbox.stories.tsx
+++ packages/core/src/Checkbox/Checkbox.stories.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { action } from '@storybook/addon-actions'
 
@@ -37,59 +37,153 @@ function onSubmit(e) {
   return formAction(e)
 }
 
-export const CheckboxStates = () => (
-  <div>
-    <Wrapper>
-      <StyledLabel htmlFor='unchecked_box'>
-        <Checkbox id='unchecked_box' onChange={checkAction} />
-        Unchecked by default
-      </StyledLabel>
-    </Wrapper>
+const FilterExample: React.FC = () => {
+  const [filterState, setFilterState] = useState({
+    all: false,
+    indeterminate: false,
+    small: false,
+    medium: false,
+    large: false,
+  })
 
-    <Wrapper>
-      <StyledLabel htmlFor='checked_box'>
-        <Checkbox id='checked_box' defaultChecked onChange={checkAction} />
-        Checked by default
-      </StyledLabel>
-    </Wrapper>
+  const handleFilterSelection = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    const target = event.target
+    if (target.id === 'all') {
+      if (filterState.all || filterState.indeterminate) {
+        setFilterState({
+          all: false,
+          indeterminate: false,
+          small: false,
+          medium: false,
+          large: false,
+        })
+      } else {
+        setFilterState({
+          all: true,
+          indeterminate: false,
+          small: true,
+          medium: true,
+          large: true,
+        })
+      }
+    } else {
+      const newFilterState = { ...filterState, [target.id]: !filterState[target.id] }
+      const indeterminate = !(
+        newFilterState.small === newFilterState.medium && newFilterState.small === newFilterState.large
+      )
+      const all = newFilterState.small && newFilterState.medium && newFilterState.large
+      setFilterState({
+        ...filterState,
+        [target.id]: !filterState[target.id],
+        indeterminate,
+        all,
+      })
+    }
+  }
+  return (
+    <>
+      <Wrapper>
+        <StyledLabel htmlFor='all'>
+          <Checkbox
+            id='all'
+            onChange={handleFilterSelection}
+            indeterminate={filterState.indeterminate}
+            checked={filterState.all}
+          />
+          Cars
+        </StyledLabel>
+        <Box ml={2}>
+          <StyledLabel htmlFor='small'>
+            <Checkbox id='small' onChange={handleFilterSelection} checked={filterState.small} />
+            Small
+          </StyledLabel>
+          <StyledLabel htmlFor='medium'>
+            <Checkbox id='medium' onChange={handleFilterSelection} checked={filterState.medium} />
+            Medium
+          </StyledLabel>
+          <StyledLabel htmlFor='large'>
+            <Checkbox id='large' onChange={handleFilterSelection} checked={filterState.large} />
+            Large
+          </StyledLabel>
+        </Box>
+      </Wrapper>
+    </>
+  )
+}
 
-    <Wrapper>
-      <StyledLabel htmlFor='disabled_box'>
-        <Checkbox id='disabled_box' disabled onChange={checkAction} />
-        <Text.span color='border.base'>Disabled</Text.span>
-      </StyledLabel>
-    </Wrapper>
+export const CheckboxStates = () => {
+  return (
+    <div>
+      <Wrapper>
+        <StyledLabel htmlFor='unchecked_box'>
+          <Checkbox id='unchecked_box' onChange={checkAction} />
+          Unchecked by default
+        </StyledLabel>
+      </Wrapper>
 
-    <Wrapper>
-      <StyledLabel htmlFor='disabled_checked_box'>
-        <Checkbox id='disabled_checked_box' disabled defaultChecked onChange={checkAction} />
-        <Text.span color='border.base'>Disabled &amp; Checked</Text.span>
-      </StyledLabel>
-    </Wrapper>
+      <Wrapper>
+        <StyledLabel htmlFor='checked_box'>
+          <Checkbox id='checked_box' defaultChecked onChange={checkAction} />
+          Checked by default
+        </StyledLabel>
+      </Wrapper>
 
-    <Wrapper title='In A Form'>
-      <form onSubmit={onSubmit}>
-        <fieldset style={{ display: 'inline-block', padding: '16px' }}>
-          <legend>Fancy Form</legend>
+      <Wrapper>
+        <StyledLabel htmlFor='disabled_box'>
+          <Checkbox id='disabled_box' disabled onChange={checkAction} />
+          <Text.span color='border.base'>Disabled</Text.span>
+        </StyledLabel>
+      </Wrapper>
 
-          <Wrapper>
-            <StyledLabel fontSize='14px' htmlFor='form_checkbox'>
-              <Checkbox id='form_checkbox' size={30} onChange={checkAction} />
-              &nbsp;In This Form
-            </StyledLabel>
-          </Wrapper>
+      <Wrapper>
+        <StyledLabel htmlFor='disabled_checked_box'>
+          <Checkbox id='disabled_checked_box' disabled defaultChecked onChange={checkAction} />
+          <Text.span color='border.base'>Disabled &amp; Checked</Text.span>
+        </StyledLabel>
+      </Wrapper>
 
-          <Button type='submit'>Submit Me</Button>
-          <br />
-          <br />
-          <Button variation='outline' color='border.base' type='reset'>
-            Reset Me
-          </Button>
-        </fieldset>
-      </form>
-    </Wrapper>
-  </div>
-)
+      <Wrapper>
+        <StyledLabel htmlFor='indeterminate_box'>
+          <Checkbox id='indeterminate_box' indeterminate onChange={checkAction} />
+          Initially indeterminate check box (clicking will checkmark it)
+        </StyledLabel>
+      </Wrapper>
+
+      <Wrapper>
+        <StyledLabel htmlFor='indeterminate_checked_box'>
+          <Checkbox id='indeterminate_checked_box' defaultChecked indeterminate onChange={checkAction} />
+          Initially indeterminate check box (clicking will uncheck it)
+        </StyledLabel>
+      </Wrapper>
+
+      <Wrapper title='Indeterminate Checkboxes'>
+        <FilterExample />
+      </Wrapper>
+
+      <Wrapper title='In A Form'>
+        <form onSubmit={onSubmit}>
+          <fieldset style={{ display: 'inline-block', padding: '16px' }}>
+            <legend>Fancy Form</legend>
+
+            <Wrapper>
+              <StyledLabel fontSize='14px' htmlFor='form_checkbox'>
+                <Checkbox id='form_checkbox' size={30} onChange={checkAction} />
+                &nbsp;In This Form
+              </StyledLabel>
+            </Wrapper>
+
+            <Button type='submit'>Submit Me</Button>
+            <br />
+            <br />
+            <Button variation='outline' color='border.base' type='reset'>
+              Reset Me
+            </Button>
+          </fieldset>
+        </form>
+      </Wrapper>
+    </div>
+  )
+}
 
 CheckboxStates.story = {
   name: 'Checkbox states',
