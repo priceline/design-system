80b9014a384e83dfd0011d50a129c0b2bc2b3494
Craig Palermo <craig.palermo@priceline.com>
2022-08-03 11:36:28 -0400

    Fix bug in 33-33-33 Carousel layout

--- packages/carousel/src/Carousel.stories.js
+++ packages/carousel/src/Carousel.stories.js
@@ -4,7 +4,7 @@ import { Flex, Card, Container, Image, Text, Box, BackgroundImage } from 'pcln-d
 import styled from 'styled-components'
 import { Carousel, ArrowButton } from '.'
 
-const SLIDE_COUNT = 10
+const SLIDE_COUNT = 6
 
 const ToutCard = styled(Card)`
   overflow: hidden;
@@ -60,8 +60,14 @@ export default {
 }
 
 function renderCards() {
-  return Array.from(Array(SLIDE_COUNT), (_, idx) => (
-    <ToutCard height={idx === 1 ? '300px' : '320px'} borderRadius={20} boxShadowSize='md' borderWidth={0}>
+  return Array.from(Array(SLIDE_COUNT)).map((_, idx) => (
+    <ToutCard
+      height={idx === 1 ? '300px' : '320px'}
+      borderRadius={20}
+      boxShadowSize='md'
+      borderWidth={0}
+      key={idx}
+    >
       <BackgroundImage height='190px' width='100%' image='https://cdn2.thecatapi.com/images/dnn.jpg' />
       <Flex color='background.lightest' p={[1, 1, 2, 2, 2, 3]}>
         <Box>
@@ -198,6 +204,16 @@ AlternateButtonColors.args = {
   showDots: false,
   showForwardBackBtns: true,
   sidePositionArrowButton: <ArrowButton {...buttonStyles} />,
+}
+
+export const CustomButtonPosition = BasicTemplate.bind({})
+CustomButtonPosition.args = {
+  visibleSlides: 3,
+  showDots: false,
+  showForwardBackBtns: true,
+  sideButtonMargin: '-50px',
+  slideSpacing: 3,
+  sidePositionArrowButton: <ArrowButton />,
 }
 
 const FixedVisibleSlidesTemplate = (args) => (
