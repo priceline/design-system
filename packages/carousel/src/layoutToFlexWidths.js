dcec77b6c51a95be2fd969cf7fba76280b11e281
Craig Palermo <3260642+craigpalermo@users.noreply.github.com>
2022-08-03 13:35:13 -0400

    Fix bug in 33-33-33 Carousel layout (#1175)

--- packages/carousel/src/layoutToFlexWidths.js
+++ packages/carousel/src/layoutToFlexWidths.js
@@ -3,7 +3,14 @@ export const layoutToFlexWidths = (layout, totalSlides) => {
     return []
   }
 
-  const layoutValues = layout?.split('-').map((i) => parseInt(i))
+  const layoutValues = layout?.split('-').map((i) => {
+    const value = parseInt(i)
+
+    // 33-33-33 is the only supported layout where values don't sum to 100. We need to
+    // account for this in order for slides not to be cut off.
+    return value === 33 ? value + 1 / 3 : value
+  })
+
   const numLayoutValues = layoutValues.length
 
   return layoutValues.reduce((acc, value) => {
