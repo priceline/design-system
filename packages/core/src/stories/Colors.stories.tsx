018e63659e8558b3fb9cb9a6317896a838c9d90d
James <62619213+pcln-james@users.noreply.github.com>
2022-08-09 15:40:31 -0500

    Support theme swapping in palette story (#1184)

--- packages/core/src/stories/Colors.stories.tsx
+++ packages/core/src/stories/Colors.stories.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import { Box, Flex, Text, createTheme } from '..'
+import { ThemeConsumer } from 'styled-components'
 
 const theme = createTheme()
 
@@ -12,17 +13,12 @@ const next = keys.map((key) => ({ key, value: theme.colors[key] }))
 
 const Chip = (props) => <Box width={1} px={5} py={4} bg={props.color} />
 
-const palette = Object.keys(theme.palette).map((key) => ({
-  key,
-  value: theme.palette[key],
-}))
-
 const ColorCard = (props) => (
   <Box>
-    <Chip name={props.name} color={props.color} />
-    <Text f={0}>{props.name}</Text>
+    <Chip name={props.shade} color={props.color} />
+    <Text f={0}>{props.shade}</Text>
     <Text as='pre' m={0}>
-      {props.color}
+      {props.hex}
     </Text>
   </Box>
 )
@@ -32,37 +28,53 @@ export default {
 }
 
 export const Palette = () => (
-  <div>
-    <Box p={3}>
-      <h1>Palette</h1>
-      <p>The palette allows you to change the color of components.</p>
-      <Flex wrap>
-        {palette.map((pal) => {
-          if (typeof pal.value === 'object') {
-            return (
-              <div style={{ width: '100%' }}>
-                <h4>{pal.key}</h4>
-                <Flex wrap>
-                  {Object.keys(pal.value).map((key) => (
-                    <Box key={key} p={3} width={[1, 1 / 2, 1 / 3, 1 / 4, 1 / 7]}>
-                      <ColorCard name={`${pal.key}.${key}`} color={theme.palette[pal.key][key]} />
+  <ThemeConsumer>
+    {(theme) => {
+      return (
+        <div>
+          <Box p={3}>
+            <h1>Palette</h1>
+            <p>The palette allows you to change the color of components.</p>
+            <Flex wrap>
+              {Object.keys(theme.palette).map((color) => {
+                const shades = theme.palette[color]
+                if (typeof shades === 'object') {
+                  return (
+                    <div style={{ width: '100%' }}>
+                      <h4>{color}</h4>
+                      <Flex wrap>
+                        {Object.keys(shades).map((shade) => {
+                          const fullColor = `${color}.${shade}`
+                          return (
+                            <Box key={fullColor} p={3} width={[1, 1 / 2, 1 / 3, 1 / 4, 1 / 7]}>
+                              <ColorCard
+                                name={fullColor}
+                                color={fullColor}
+                                shade={shade}
+                                hex={shades[shade]}
+                              />
+                            </Box>
+                          )
+                        })}
+                      </Flex>
+                      <hr />
+                    </div>
+                  )
+                } else {
+                  const fullColor = `${color}.${shades}`
+                  return (
+                    <Box key={fullColor} p={3} width={[1, 1 / 2, 1 / 3, 1 / 4, 1 / 5]}>
+                      <ColorCard name={fullColor} color={fullColor} />
                     </Box>
-                  ))}
-                </Flex>
-                <hr />
-              </div>
-            )
-          } else {
-            return (
-              <Box key={pal.key} p={3} width={[1, 1 / 2, 1 / 3, 1 / 4, 1 / 5]}>
-                <ColorCard name={pal.key} color={pal.value} />
-              </Box>
-            )
-          }
-        })}
-      </Flex>
-    </Box>
-  </div>
+                  )
+                }
+              })}
+            </Flex>
+          </Box>
+        </div>
+      )
+    }}
+  </ThemeConsumer>
 )
 
 export const Colors = () => (
@@ -73,7 +85,7 @@ export const Colors = () => (
     <Flex wrap>
       {next.map((color) => (
         <Box key={color.key} p={3} width={[1, 1 / 2, 1 / 3, 1 / 4, 1 / 7]}>
-          <ColorCard name={color.key} color={color.value} />
+          <ColorCard name={color.key} color={color.value} shade={color.key} hex={color.value} />
         </Box>
       ))}
     </Flex>
