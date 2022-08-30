759614938651d4d2a469fc4d0c034ab86e5cfef2
Ivan Valadares <ivan.valadares@priceline.com>
2022-08-30 11:03:06 -0400

    fix(PKG-16093): refactor tests

--- packages/carousel/src/helpers.spec.js
+++ packages/carousel/src/helpers.spec.js
@@ -2,6 +2,20 @@ import React from 'react'
 import { render, waitFor } from 'testing-library'
 import { getVisibleSlidesArray, useResponsiveVisibleSlides } from './helpers'
 
+const resizeWindow = (width) => {
+  // Simulate window resize event
+  const resizeEvent = document.createEvent('Event')
+  resizeEvent.initEvent('resize', true, true)
+  global.window.innerWidth = width
+  global.window.dispatchEvent(resizeEvent)
+}
+
+const input = [1, 2, 3]
+const TextVisibleSlides = () => {
+  const visibleSlides = useResponsiveVisibleSlides(input)
+  return <div>{visibleSlides} Slide(s)</div>
+}
+
 describe('getVisibleSlidesArray', () => {
   it('Pass an array, return same input', () => {
     const input = [2, 2, 3]
@@ -15,19 +29,9 @@ describe('getVisibleSlidesArray', () => {
 })
 
 describe('useResponsiveVisibleSlides', () => {
-  const resizeWindow = (width) => {
-    // Simulate window resize event
-    const resizeEvent = document.createEvent('Event')
-    resizeEvent.initEvent('resize', true, true)
-    global.window.innerWidth = width
-    global.window.dispatchEvent(resizeEvent)
-  }
-
-  const input = [1, 2, 3]
-  const TextVisibleSlides = () => {
-    const visibleSlides = useResponsiveVisibleSlides(input)
-    return <div>{visibleSlides} Slide(s)</div>
-  }
+  beforeEach(() => {
+    resizeWindow(1400)
+  })
 
   it('Resize Browser Width to Medium', async () => {
     const { getByText } = render(<TextVisibleSlides />)
@@ -42,7 +46,7 @@ describe('useResponsiveVisibleSlides', () => {
 
   it('Resize Browser Width to Small', async () => {
     const { getByText } = render(<TextVisibleSlides />)
-    expect(getByText('2 Slide(s)')).toBeInTheDocument()
+    expect(getByText('3 Slide(s)')).toBeInTheDocument()
 
     resizeWindow(600)
 
