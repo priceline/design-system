d0eb9cb8c7c2a6faf79a2f4bbd50ec033e188bd3
Ivan Valadares <ivan.valadares@priceline.com>
2022-08-30 10:38:05 -0400

    fix(PKG-16093): refactor tests

--- packages/carousel/src/helpers.spec.js
+++ packages/carousel/src/helpers.spec.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render, waitFor } from 'testing-library'
 import { getVisibleSlidesArray, useResponsiveVisibleSlides } from './helpers'
-import { act } from 'react-dom/test-utils'
 
 describe('getVisibleSlidesArray', () => {
   it('Pass an array, return same input', () => {
@@ -20,26 +19,35 @@ describe('useResponsiveVisibleSlides', () => {
     // Simulate window resize event
     const resizeEvent = document.createEvent('Event')
     resizeEvent.initEvent('resize', true, true)
-    act(() => {
-      global.window.innerWidth = width
-      global.window.dispatchEvent(resizeEvent)
-    })
+    global.window.innerWidth = width
+    global.window.dispatchEvent(resizeEvent)
   }
 
   const input = [1, 2, 3]
   const TextVisibleSlides = () => {
     const visibleSlides = useResponsiveVisibleSlides(input)
-    return <div className={`visible_${visibleSlides}`}>{visibleSlides} Slide(s)</div>
+    return <div>{visibleSlides} Slide(s)</div>
   }
 
-  it('Resize Browser Width', async () => {
-    const { container } = render(<TextVisibleSlides />)
-    expect(container.querySelector('.visible_3')).toBeInTheDocument()
+  it('Resize Browser Width to Medium', async () => {
+    const { getByText } = render(<TextVisibleSlides />)
+    expect(getByText('3 Slide(s)')).toBeInTheDocument()
+
+    resizeWindow(1000)
+
+    await waitFor(() => {
+      expect(getByText('2 Slide(s)')).toBeInTheDocument()
+    })
+  })
+
+  it('Resize Browser Width to Small', async () => {
+    const { getByText } = render(<TextVisibleSlides />)
+    expect(getByText('2 Slide(s)')).toBeInTheDocument()
 
     resizeWindow(600)
 
     await waitFor(() => {
-      expect(container.querySelector('.visible_1')).toBeInTheDocument()
+      expect(getByText('1 Slide(s)')).toBeInTheDocument()
     })
   })
 })
