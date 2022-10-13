6e763e1e99281f8eb76645ba3ec0d2109ae82e59
Devin Efendy <90356592+devin-efendy-pcln@users.noreply.github.com>
2022-10-13 12:12:27 -0500

    Feat/ppn 58101 create swatch component (#1207)
    * feat(ppn-54380): create Swatch component
    
    * add documentation to docsite
    
    * add pointer cursor and the rest of the props
    
    * use cursor default if onClick not provided

---
+++ packages/core/src/Swatch/Swatch.spec.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Swatch } from '.'
+import { render, fireEvent } from '../__test__/testing-library'
+
+const mockColors = ['red', 'green', 'blue']
+
+describe('Swatch', () => {
+  it('should render correctly', () => {
+    const { getByTestId } = render(<Swatch colors={mockColors} />)
+
+    mockColors.map((color, idx) => {
+      expect(getByTestId(`${idx}-${color}`)).toHaveStyle(`background-color: ${color}`)
+    })
+  })
+
+  it('should call onClick callback with selected color value', () => {
+    const mockOnClick = jest.fn()
+
+    const { getByTestId } = render(<Swatch colors={mockColors} onClick={mockOnClick} />)
+
+    mockColors.map((color, idx) => {
+      expect(getByTestId(`${idx}-${color}`)).toHaveStyle('cursor: pointer')
+
+      fireEvent.click(getByTestId(`${idx}-${color}`))
+      expect(mockOnClick).toHaveBeenLastCalledWith(color)
+    })
+  })
+
+  it('does not crash when onClick callback is not provided', () => {
+    const mockOnClick = jest.fn()
+
+    const { getByTestId } = render(<Swatch colors={mockColors} />)
+
+    mockColors.map((color, idx) => {
+      expect(getByTestId(`${idx}-${color}`)).toHaveStyle('cursor: default')
+
+      fireEvent.click(getByTestId(`${idx}-${color}`))
+      expect(mockOnClick).toHaveBeenCalledTimes(0)
+    })
+  })
+})
