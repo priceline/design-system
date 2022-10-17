3b19c253515a2d80c6f6ccba9bb425a92246f2ec
rhunt <ryan.hunt@priceline.com>
2022-10-17 17:39:55 -0400

    SlideBox modify default mobile sizes

--- packages/core/src/SlideBox/helpers.spec.ts
+++ packages/core/src/SlideBox/helpers.spec.ts
@@ -5,15 +5,15 @@ describe('getVisibleSlidesArray', () => {
     expect(getVisibleSlides([2, 4, 8])).toEqual([1 / 2, 1 / 4, 1 / 8])
   })
   it('int visible slides', () => {
-    expect(getVisibleSlides(3)).toEqual([1 / 1.2, 1 / 2.2, 1 / 3])
+    expect(getVisibleSlides(3)).toEqual([1 / 1.2, null, null, 1 / 2.2, 1 / 3])
   })
 })
 
 describe('getCustomWidths', () => {
   it('First slide', () => {
-    expect(getCustomWidths([60, 40], 0)).toEqual([1 / 1.2, 1 / 2.2, '60%'])
+    expect(getCustomWidths([60, 40], 0)).toEqual([1 / 1.2, null, null, 1 / 2.2, '60%'])
   })
   it('Second slide', () => {
-    expect(getCustomWidths([60, 40], 1)).toEqual([1 / 1.2, 1 / 2.2, '40%'])
+    expect(getCustomWidths([60, 40], 1)).toEqual([1 / 1.2, null, null, 1 / 2.2, '40%'])
   })
 })
--- packages/core/src/SlideBox/helpers.ts
+++ packages/core/src/SlideBox/helpers.ts
@@ -1,15 +1,15 @@
-import { MAX_NUM_SLIDES_XS, MAX_NUM_SLIDES_SM } from './constants'
-
 const getWidth = (visibleSlides) => 1 / visibleSlides
 
 const getVisibleSlides = (visibleSlides) =>
   Array.isArray(visibleSlides)
     ? visibleSlides.map((vs) => getWidth(vs))
-    : [getWidth(MAX_NUM_SLIDES_XS), getWidth(MAX_NUM_SLIDES_SM), getWidth(visibleSlides)]
+    : [getWidth(1.2), null, null, getWidth(2.2), getWidth(visibleSlides)]
 
 const getCustomWidths = (widths, index) => [
-  getWidth(MAX_NUM_SLIDES_XS),
-  getWidth(MAX_NUM_SLIDES_SM),
+  getWidth(1.2),
+  null,
+  null,
+  getWidth(2.2),
   `${widths[(index + widths.length) % widths.length]}%`,
 ]
 
