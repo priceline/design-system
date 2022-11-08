db3abe3fc2da330c22be4becb3d5ebc6b19220b6
James Stewart <james.stewart@priceline.com>
2022-11-08 13:29:47 -0600

    Update link and textColorOn disabled states for v4

---
+++ packages/core/src/__test__/mocks/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  primary: 'primary',
+  secondary: 'secondary',
+  text: 'text',
+  success: 'success',
+  error: 'error',
+  warning: 'warning',
+  alert: 'alert',
+  caution: 'caution',
+  notify: 'notify',
+  pricePrimary: 'pricePrimary',
+  priceSecondary: 'priceSecondary',
+  promoPrimary: 'promoPrimary',
+  promoSecondary: 'promoSecondary',
+  border: 'border',
+  background: 'background',
+}
