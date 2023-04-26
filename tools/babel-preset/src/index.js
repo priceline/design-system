70724899adbc69cc8e5e11659eb0eb669fb3a740
Steve Dalonzo <steven.dalonzo@priceline.com>
2023-04-25 22:37:11 -0400

    clean up of styled-system peers

--- tools/babel-preset/src/index.js
+++ tools/babel-preset/src/index.js
@@ -17,15 +17,7 @@ module.exports = () => ({
       },
     ],
   ],
-  plugins: [
-    require('babel-plugin-styled-components'),
-    require('@babel/plugin-proposal-class-properties'),
-    require('@babel/plugin-proposal-nullish-coalescing-operator'),
-    require('@babel/plugin-proposal-optional-chaining'),
-    require('@babel/plugin-syntax-dynamic-import'),
-    require('@babel/plugin-proposal-export-default-from'),
-    require('babel-plugin-date-fns'),
-  ],
+  plugins: [require('babel-plugin-styled-components')],
   env: {
     test: {
       presets: [
@@ -38,17 +30,8 @@ module.exports = () => ({
           },
         ],
       ],
-      plugins: [
-        require('babel-plugin-styled-components'),
-        require('@babel/plugin-proposal-nullish-coalescing-operator'),
-        require('@babel/plugin-proposal-optional-chaining'),
-        require('babel-plugin-dynamic-import-node'),
-        require('@babel/plugin-transform-modules-commonjs'),
-      ],
+      plugins: [require('babel-plugin-styled-components')],
       compact: true,
-    },
-    production: {
-      plugins: [require('babel-plugin-transform-react-remove-prop-types')],
     },
     cjs: {
       presets: [[require('@babel/preset-env'), { modules: 'cjs' }]],
