6c10574a9a920187e574bf14346df8bc3d6f85cb
Justin Trenary <justin.trenary@priceline.com>
2023-05-25 14:18:25 -0400

    fix unit tests

--- packages/core/src/Grid/Grid.spec.tsx
+++ packages/core/src/Grid/Grid.spec.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { Grid, IGridProps } from './Grid'
 
 describe('Grid', () => {
-  const defaultProps: IGridProps = {
+  const testProps: IGridProps = {
     gap: '10px',
     rowGap: '20px',
     columnGap: '30px',
@@ -25,22 +25,22 @@ describe('Grid', () => {
   })
 
   it('applies grid styles correctly', () => {
-    const { container } = render(<Grid {...defaultProps} />)
+    const { container } = render(<Grid {...testProps} />)
     const gridElement = container.firstChild
 
-    expect(gridElement).toHaveStyle('display: grid')
-    expect(gridElement).toHaveStyle('grid-gap: 10px')
-    expect(gridElement).toHaveStyle('grid-row-gap: 20px')
-    expect(gridElement).toHaveStyle('grid-column-gap: 30px')
-    expect(gridElement).toHaveStyle('grid-column: 1 / 3')
-    expect(gridElement).toHaveStyle('grid-row: 1 / span 2')
-    expect(gridElement).toHaveStyle('grid-area: header')
-    expect(gridElement).toHaveStyle('grid-auto-flow: row dense')
-    expect(gridElement).toHaveStyle('grid-auto-rows: minmax(100px, auto)')
-    expect(gridElement).toHaveStyle('grid-auto-columns: repeat(2, 1fr)')
-    expect(gridElement).toHaveStyle('grid-template-rows: repeat(2, 1fr)')
-    expect(gridElement).toHaveStyle('grid-template-columns: repeat(3, 1fr)')
-    expect(gridElement).toHaveStyle('grid-template-areas: "header header header" "main main sidebar"')
-    expect(gridElement).toHaveStyle('place-items: center')
+    expect(gridElement).toHaveStyleRule('display: grid')
+    expect(gridElement).toHaveStyleRule('grid-gap: 10px')
+    expect(gridElement).toHaveStyleRule('grid-row-gap: 20px')
+    expect(gridElement).toHaveStyleRule('grid-column-gap: 30px')
+    expect(gridElement).toHaveStyleRule('grid-column: 1 / 3')
+    expect(gridElement).toHaveStyleRule('grid-row: 1 / span 2')
+    expect(gridElement).toHaveStyleRule('grid-area: header')
+    expect(gridElement).toHaveStyleRule('grid-auto-flow: row dense')
+    expect(gridElement).toHaveStyleRule('grid-auto-rows: minmax(100px, auto)')
+    expect(gridElement).toHaveStyleRule('grid-auto-columns: repeat(2, 1fr)')
+    expect(gridElement).toHaveStyleRule('grid-template-rows: repeat(2, 1fr)')
+    expect(gridElement).toHaveStyleRule('grid-template-columns: repeat(3, 1fr)')
+    expect(gridElement).toHaveStyleRule('grid-template-areas: "header header header" "main main sidebar"')
+    expect(gridElement).toHaveStyleRule('place-items: center')
   })
 })
