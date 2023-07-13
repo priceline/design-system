8ae217a3104dc348709b44824bb99bad4b35ff00
Brandon Soong <bsoong99@gmail.com>
2023-07-13 16:26:49 -0400

    removing unused values

--- packages/core/src/Tab/Tab.spec.tsx
+++ packages/core/src/Tab/Tab.spec.tsx
@@ -2,37 +2,6 @@ import React from 'react'
 import { render, screen } from '../__test__/testing-library'
 import { userEvent } from '@storybook/testing-library'
 import { Tab } from '.'
-import { DefaultContent, DefaultContent2, DefaultContent3 } from './constants'
-
-const tabsContent = [
-  {
-    id: 't1',
-    children: <DefaultContent />,
-  },
-  {
-    id: 't2',
-    children: <DefaultContent2 />,
-  },
-  {
-    id: 't3',
-    children: <DefaultContent3 />,
-  },
-]
-
-const tabsData = [
-  {
-    id: 't1',
-    text: 'Tab 1',
-  },
-  {
-    id: 't2',
-    text: 'Tab 2',
-  },
-  {
-    id: 't3',
-    text: 'Tab 3',
-  },
-]
 
 describe('Tab', () => {
   const tabsContent = [
