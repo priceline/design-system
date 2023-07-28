4ab2b5fd374484e80ec33fc46bfd062378d0f2dd
Caleb Liu <120677881+CalebBoLiuPriceline@users.noreply.github.com>
2023-07-28 11:37:39 -0400

    feat(AIR-56045): add light and dark props to shimmer (#1374)
    * feat(AIR-56045): add light and dark props to shimmer
    
    * add change
    
    * feat(AIR-56045): add dark box to the dark Shimmer story
    
    * add change
    
    * rename origin with base on Shimmer
    
    * add change

--- packages/core/src/Shimmer/Shimmer.spec.tsx
+++ packages/core/src/Shimmer/Shimmer.spec.tsx
@@ -8,7 +8,27 @@ describe('Shimmer', () => {
     const wrapper = getByTestId('Shimmer__Wrapper')
     const glare = getByTestId('Shimmer__Glare')
 
-    expect(wrapper).toHaveStyleRule('background-color', '#eee')
+    expect(wrapper).toHaveStyleRule('background-color', '#EEEEEE')
+    expect(wrapper).toHaveStyleRule('height', '50px')
+    expect(wrapper).toHaveStyleRule('width', '100px')
+    expect(glare).toHaveStyleRule('animation', expect.stringContaining('1.2s ease-in-out infinite'))
+  })
+
+  test('should render light variation correctly', () => {
+    const { getByTestId } = render(<Shimmer height='50px' width='100px' variation='light' />)
+    const wrapper = getByTestId('Shimmer__Wrapper')
+    const glare = getByTestId('Shimmer__Glare')
+
+    expect(wrapper).toHaveStyleRule('background-color', '#00000014')
+    expect(glare).toHaveStyleRule('animation', expect.stringContaining('1.2s ease-in-out infinite'))
+  })
+
+  test('should render dark variation correctly', () => {
+    const { getByTestId } = render(<Shimmer height='50px' width='100px' variation='dark' />)
+    const wrapper = getByTestId('Shimmer__Wrapper')
+    const glare = getByTestId('Shimmer__Glare')
+
+    expect(wrapper).toHaveStyleRule('background-color', '#FFFFFF14')
     expect(wrapper).toHaveStyleRule('height', '50px')
     expect(wrapper).toHaveStyleRule('width', '100px')
     expect(glare).toHaveStyleRule('animation', expect.stringContaining('1.2s ease-in-out infinite'))
