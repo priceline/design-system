627ed0261d54cad2b1d02932d40643a03f1607e7
Jordon Hong <jordon.hong@priceline.com>
2024-02-07 14:57:54 -0600

    add inner content to popover dialog story

--- packages/popover/src/Popover/Popover.stories.jsx
+++ packages/popover/src/Popover/Popover.stories.jsx
@@ -142,13 +142,7 @@ export const bottom = () => (
 export const InDialog = () => (
   <Dialog open size='md' zIndex={0}>
     <Box height={300}>
-      <Popover
-        renderContent={SimpleTextContent}
-        placement='bottom'
-        ariaLabel='Dialog Popover'
-        idx={2}
-        width={400}
-      >
+      <Popover renderContent={InnerContent} placement='bottom' ariaLabel='Dialog Popover' idx={2} width={400}>
         <Button m={3}>Open Popover</Button>
       </Popover>
     </Box>
