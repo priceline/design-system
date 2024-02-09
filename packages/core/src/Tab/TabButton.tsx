da7af2a0742177ab10ddc4904c458242372cb592
Brandon Soong <brandon.soong@priceline.com>
2024-02-09 18:12:14 -0500

    adding ternary for icon logic

--- packages/core/src/Tab/TabButton.tsx
+++ packages/core/src/Tab/TabButton.tsx
@@ -3,6 +3,7 @@ import { StyledText, TabTriggerButton } from './Tab.styled'
 import { Flex } from '../Flex/Flex'
 import { Box } from '../Box/Box'
 import type { TabProps, MappedTabProps } from './Tab'
+import { Grid } from '../Grid/Grid'
 export const TabButton = ({ border, tab, size, isTransparent }: Partial<TabProps> & MappedTabProps) => {
   const [isHover, setIsHover] = React.useState(false)
   return (
@@ -15,17 +16,24 @@ export const TabButton = ({ border, tab, size, isTransparent }: Partial<TabProps
       value={`tab-${tab.id}`}
       key={`tab-${tab.id}`}
     >
-      {tab.icon && (
-        <Box ml={3} size={24}>
-          {React.createElement(tab.icon, {
-            title: tab.icon.name,
-            size: 24,
-          })}
-        </Box>
+      {tab.icon ? (
+        <Grid alignItems='center' templateColumns='auto 1fr'>
+          <Box ml={3} size={24}>
+            {React.createElement(tab.icon, {
+              title: tab.icon.name,
+              size: 24,
+            })}
+          </Box>
+
+          <StyledText size={size} style={{ fontFamily: 'Montserrat' }} textStyle='paragraph'>
+            {tab.text}
+          </StyledText>
+        </Grid>
+      ) : (
+        <StyledText size={size} style={{ fontFamily: 'Montserrat' }} textStyle='paragraph'>
+          {tab.text}
+        </StyledText>
       )}
-      <StyledText size={size} style={{ fontFamily: 'Montserrat' }} textStyle='paragraph'>
-        {tab.text}
-      </StyledText>
     </TabTriggerButton>
   )
 }
