cc3ca8e93ae55c0a7b72dab8a043a1a4d97b1a3c
Brandon Soong <brandon.soong@priceline.com>
2024-03-29 11:53:40 -0400

    making the logic not falsy

--- packages/core/src/Dialog/Dialog.styled.tsx
+++ packages/core/src/Dialog/Dialog.styled.tsx
@@ -255,7 +255,7 @@ export const DialogContent = ({
               )}
             </SmoothTransitionBox>
           ) : (
-            <Box height='100%' style={innerContentScroll && { overflowY: 'scroll' }}>
+            <Box height='100%' style={innerContentScroll ? { overflowY: 'scroll' } : {}}>
               {children}
             </Box>
           )}
