97dd4c2c3cc7c139afee4f2090686000a4a0a251
Kevin <83264008+coolestKev@users.noreply.github.com>
2024-12-20 09:00:38 -0500

    task(HTL-113265): Update unit test

--- packages/core/src/Drawer/hooks/useSnap.spec.ts
+++ packages/core/src/Drawer/hooks/useSnap.spec.ts
@@ -2,27 +2,27 @@ import { useSnap } from './useSnap'
 import { renderHook } from '@testing-library/react'
 
 describe('Drawer snap hook unit test', () => {
-  const snapHeights = ['0%', '20%', '30%']
+  const snapHeights = ['20%', '0%', '20%']
   const snapDimensions = ['100%', '100%', '100%']
   const { result } = renderHook(() => useSnap(snapHeights, snapDimensions))
   const { snapPosition, handleSnap } = result.current
   test('Snap position initialized correctly', () => {
     // Start middle, scroll up and scroll down should be back to initial position
-    expect(snapPosition).toBe('20%')
+    expect(snapPosition).toBe('0%')
     // Scroll to top
     handleSnap({ pointerType: 'touch', type: 'pointerup' }, { offset: { y: 101 } })
     // Scroll back to middle
     handleSnap({ pointerType: 'touch', type: 'pointerup' }, { offset: { y: -101 } })
     // Expect it to be back in middle
-    expect(snapPosition).toBe('20%')
+    expect(snapPosition).toBe('0%')
   })
 
   test('Does not snap on pointer cancel events (e.g. scrolling through drawer content on iphone)', () => {
     // Start middle, and touch scroll on drawer content should not trigger an action
-    expect(snapPosition).toBe('20%')
+    expect(snapPosition).toBe('0%')
     // Scroll to top
     handleSnap({ pointerType: 'touch', type: 'pointercancel' }, { offset: { y: 101 } })
     // Expect no change
-    expect(snapPosition).toBe('20%')
+    expect(snapPosition).toBe('0%')
   })
 })
