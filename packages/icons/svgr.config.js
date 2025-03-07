a9ac9f93939379df68245ec6361457741443544a
Remy Gratwohl <remy.gratwohl@priceline.com>
2025-03-07 11:16:03 -0500

    fix(UXPT-9366): revert svgr change

--- packages/icons/svgr.config.js
+++ packages/icons/svgr.config.js
@@ -35,6 +35,14 @@ const ${componentName} = styled(BaseComponent)\`
 
 ${componentName}.isIcon = true
 
+${componentName}.defaultProps = {
+  size: 24,
+  tabIndex: -1,
+  focusable: false,
+  'aria-hidden': true,
+  role: 'img'
+}
+
 export default ${componentName}`
 
 module.exports = {
